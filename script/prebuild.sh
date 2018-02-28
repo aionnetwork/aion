8bc9ad00d26c4a016b905072557f8eb94f90a766
AionJayT <jay@nuco.io>
2018-02-28 13:19:39 -0500

    update ant build script - pack docs, modify kernel version by the git revision during the pack_build

---
+++ script/prebuild.sh
@@ -0,0 +1,7 @@
+# append the git revision into the kernel version number
+GITVER=$(git log -1 --format="%h")
+TOKEN="public static final String VERSION ="
+sed -i -r "/$TOKEN/ s/.{2}$//" ./modAionImpl/src/org/aion/zero/impl/AionHub.java
+sed -i "/$TOKEN/ s/$/.$GITVER\";/" ./modAionImpl/src/org/aion/zero/impl/AionHub.java
+
+head -80 ./modAionImpl/src/org/aion/zero/impl/AionHub.java
