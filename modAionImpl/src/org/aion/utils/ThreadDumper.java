44f8aeb4dc00c526e8a3d152ce999124265d7bbd
Alexandra <alexandra@nuco.io>
2018-03-20 15:13:19 -0400

    adding stack trace to the thread dumps

--- modAionImpl/src/org/aion/utils/ThreadDumper.java
+++ modAionImpl/src/org/aion/utils/ThreadDumper.java
@@ -22,6 +22,7 @@
  ******************************************************************************/
 package org.aion.utils;
 
+import java.lang.management.LockInfo;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
@@ -34,14 +35,28 @@ public class ThreadDumper {
         final StringBuilder sb = new StringBuilder();
         final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
         for (Thread t : Thread.getAllStackTraces().keySet()) {
-            ThreadInfo threadInfo = threadMXBean.getThreadInfo(t.getId());
-            // sb.append("{");
-            // sb.append("name = ").append(t.getName()).append(", ");
-            // sb.append("id = ").append(t.getId()).append(", ");
-            // sb.append("state = ").append(threadInfo.getThreadState()).append(", ");
-            // sb.append("lockInfo = ").append(threadInfo.getLockInfo());
-            // sb.append("}\n");
-            sb.append(threadInfo.toString());
+            if (t.isAlive()) {
+                ThreadInfo threadInfo = threadMXBean.getThreadInfo(t.getId());
+                sb.append(threadInfo.toString());
+                sb.deleteCharAt(sb.length() - 1);
+                for (StackTraceElement ste : t.getStackTrace()) {
+                    sb.append("\tat ").append(ste.getClassName()).append(".").append(ste.getMethodName()).append("(")
+                            .append(ste.getFileName()).append(":").append(ste.getLineNumber()).append(")");
+                    sb.append("\n");
+                }
+                sb.append("Ownable synchronizers:");
+                LockInfo[] s = threadInfo.getLockedSynchronizers();
+                if (s == null || s.length == 0) {
+                    sb.append(" None.\n");
+                } else {
+                    sb.append("\n");
+                    for (final LockInfo lockInfo : s) {
+                        sb.append(lockInfo.getClassName()).append(" <").append(lockInfo.getIdentityHashCode())
+                                .append("> \n");
+                    }
+                }
+                sb.append("\n");
+            }
         }
         return sb.toString();
     }
