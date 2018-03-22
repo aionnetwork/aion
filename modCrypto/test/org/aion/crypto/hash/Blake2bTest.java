a3f6203c2b6936057e12fd6719ffcb0f2ad00dd5
aion-jin <aion.jinius@gmail.com>
2018-03-22 19:21:06 -0400

    blake2b, load long M[i] from bytebuffer, will increase little bit ( 1.1% ) in whole compress function.

--- modCrypto/test/org/aion/crypto/hash/Blake2bTest.java
+++ modCrypto/test/org/aion/crypto/hash/Blake2bTest.java
@@ -1,7 +1,5 @@
 package org.aion.crypto.hash;
 
-import static org.junit.Assert.*;
-
 import org.aion.crypto.hash.Blake2b.Engine;
 import org.junit.Test;
 
@@ -10,28 +8,31 @@ public class Blake2bTest {
 	@Test
 	public void test() {
 		byte[] in = new byte[16 * 8 +3];
-//		for (int i = 0; i < 64; i++) {
-//			in[i] = (byte) i;
-//		}
+
 		Engine e = new Engine();
 
 		long ts0 = 0;
 		long ts1 = 0;
 		long tt0, tt1;
 
+		tt0 = System.nanoTime();
+		
 		for (int i = 0; i < 100000; i++) {
-			tt0 = System.nanoTime();
+			
 			e.USE_BB_BS2LONG_CONVERT = true;
-			tt1 = System.nanoTime();
 			e.compress(in, 3);
+			
+			tt1 = System.nanoTime();
 			ts0 += tt1 - tt0;
+			
 			e.USE_BB_BS2LONG_CONVERT = false;
 			e.compress(in, 3);
+			
 			tt0 = System.nanoTime();
 			ts1 += tt0 - tt1;
 		}
 
-		//  T0:4693174 T1:152919871 T0:T1_0.030690413   ,  use BB convert byte[] to long will be 30x faster.
+		//   T0:77810133 T1:79152316 T0:T1_0.983043 ,  load M[i] from BB will increase little bit perf.  
 		System.out.println(" T0:" + ts0 + " T1:" + ts1 + " T0:T1_" + (float)ts0 / ts1);
 	}
 
