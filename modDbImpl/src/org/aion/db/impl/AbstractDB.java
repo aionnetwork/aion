2adcbbf72d544b4eda9c261f4078ebcbef4e9df9
Yulong Wu <yulong@nuco.io>
2018-03-26 15:29:34 -0400

    Increase the default db cache size and write buffer size

--- modDbImpl/src/org/aion/db/impl/AbstractDB.java
+++ modDbImpl/src/org/aion/db/impl/AbstractDB.java
@@ -60,8 +60,8 @@ public abstract class AbstractDB implements IByteArrayKeyValueDatabase {
 
     protected final ReadWriteLock lock = new ReentrantReadWriteLock();
 
-    protected static final int DEFAULT_CACHE_SIZE_BYTES = 128 * 1024 * 1024; // 128mb
-    protected static final int DEFAULT_WRITE_BUFFER_SIZE_BYTES = 10 * 1024 * 1024; // 10mb
+    protected static final int DEFAULT_CACHE_SIZE_BYTES = 512 * 1024 * 1024; // 512mb
+    protected static final int DEFAULT_WRITE_BUFFER_SIZE_BYTES = 16 * 1024 * 1024; // 16mb
 
     protected final String name;
     protected String path = null;
