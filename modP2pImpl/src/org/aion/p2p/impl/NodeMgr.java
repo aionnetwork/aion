1e7790c73fdd50ebc8b844e2ba3f60942961894a
Yulong Wu <yulong@nuco.io>
2018-04-03 13:22:42 -0400

    Re-adjust timeout parameters as network issue resolved

--- modP2pImpl/src/org/aion/p2p/impl/NodeMgr.java
+++ modP2pImpl/src/org/aion/p2p/impl/NodeMgr.java
@@ -369,7 +369,7 @@ public class NodeMgr implements INodeMgr {
         long now = System.currentTimeMillis();
 
         OptionalDouble average = activeNodes.values().stream().mapToLong(n -> now - n.getTimestamp()).average();
-        double timeout = average.orElse(20000) * 3;
+        double timeout = average.orElse(4000) * 5;
         if (pmgr.showLog) {
             System.out.printf("<p2p average-delay=%.0fms>\n", average.orElse(0));
         }
