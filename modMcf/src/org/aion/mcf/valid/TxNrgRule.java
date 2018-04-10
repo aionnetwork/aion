b175fcf76ba2072fbdd073043587976bf46944cf
AionJayT <jay@nuco.io>
2018-04-10 18:20:50 -0400

    refine TxNrgRule

--- modMcf/src/org/aion/mcf/valid/TxNrgRule.java
+++ modMcf/src/org/aion/mcf/valid/TxNrgRule.java
@@ -1,12 +1,15 @@
 package org.aion.mcf.valid;
 
+import org.aion.mcf.vm.Constants;
+
 public class TxNrgRule {
-    private static final long CONTRACT_CREATE_TX_NRG_MAX = 5_000_001;
-    private static final long TX_NRG_MAX = 2_000_001;
-    private static final long TX_NRG_MIN = 20_999;
+    private static final long CONTRACT_CREATE_TX_NRG_MAX = Constants.NRG_TX_CREATE_MAX + 1;
+    private static final long CONTRACT_CREATE_TX_NRG_MIN = Constants.NRG_TX_CREATE - 1;
+    private static final long TX_NRG_MAX = Constants.NRG_TRANSACTION_MAX + 1;
+    private static final long TX_NRG_MIN = Constants.NRG_TRANSACTION - 1;
 
     public static boolean isValidNrgContractCreate(long nrg) {
-        return (nrg > TX_NRG_MIN) && (nrg < CONTRACT_CREATE_TX_NRG_MAX) ;
+        return (nrg > CONTRACT_CREATE_TX_NRG_MIN) && (nrg < CONTRACT_CREATE_TX_NRG_MAX) ;
     }
 
     public static boolean isValidNrgTx(long nrg) {
