f30617fdc8bdd7fa0c444411dcbdf11d7e13ea17
Ali Sharif <ali@nuco.io>
2018-04-12 16:07:56 -0400

    operational api enhancements

--- modApiServer/src/org/aion/api/server/rpc/RpcMethods.java
+++ modApiServer/src/org/aion/api/server/rpc/RpcMethods.java
@@ -78,6 +78,8 @@ public class RpcMethods {
     private final Map<String, RpcMethod> ops = Map.ofEntries(
             Map.entry("ops_getAccountState", (params) -> api.ops_getAccountState(params)),
             Map.entry("ops_getChainHeadViewBestBlock", (params) -> api.ops_getChainHeadViewBestBlock()),
+            Map.entry("ops_getTransaction", (params) -> api.ops_getTransaction(params)),
+            Map.entry("ops_getBlock", (params) -> api.ops_getBlock(params)),
             Map.entry("ops_getChainHeadView", (params) -> api.ops_getChainHeadView()),
             Map.entry("eth_getBalance", (params) -> api.eth_getBalance(params)),
             Map.entry("eth_sendRawTransaction", (params) -> api.eth_sendRawTransaction(params)),
