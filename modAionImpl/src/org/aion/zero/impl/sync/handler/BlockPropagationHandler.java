11fc3d993d2623c19e717b4c46ab30acb819979e
qoire <mr.sun.yao@gmail.com>
2018-04-14 10:08:21 -0400

    relax conditions for propr

--- modAionImpl/src/org/aion/zero/impl/sync/handler/BlockPropagationHandler.java
+++ modAionImpl/src/org/aion/zero/impl/sync/handler/BlockPropagationHandler.java
@@ -207,7 +207,7 @@ public class BlockPropagationHandler {
                 // peer is within 5 blocks of the block we're about to send
                 .filter(n -> {
                     long delta = block.getNumber() - n.getBestBlockNumber();
-                    return delta >= 0 && delta <= 5;
+                    return delta >= 0 && delta <= 100;
                 })
                 .forEach(n -> {
                     if (log.isDebugEnabled())
