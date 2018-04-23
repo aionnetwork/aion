6396a33d95cc94d96821e340839366e423cf6fea
Alexandra <alexandra@nuco.io>
2018-04-23 17:00:32 -0400

    simplifying the database configuration by reducing the number of options for non-experts

--- modMcf/src/org/aion/mcf/config/CfgDb.java
+++ modMcf/src/org/aion/mcf/config/CfgDb.java
@@ -22,6 +22,9 @@
  ******************************************************************************/
 package org.aion.mcf.config;
 
+import org.aion.base.util.Utils;
+import org.aion.db.impl.DBVendor;
+
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
@@ -57,7 +60,14 @@ public class CfgDb {
     }
 
     private String path;
+    private String vendor;
+    private boolean compression;
 
+    /**
+     * Enabling expert mode allows more detailed database configurations.
+     *
+     * @implNote This parameter must remain hardcoded.
+     */
     private boolean expert = false;
 
     // individual db configurations
@@ -65,13 +75,16 @@ public class CfgDb {
 
     public CfgDb() {
         this.path = "database";
-        this.specificConfig = new HashMap<>();
-        this.specificConfig.put(Names.DEFAULT, new CfgDbDetails());
+        this.vendor = DBVendor.LEVELDB.toValue();
+        this.compression = false;
+
+        if (expert) {
+            this.specificConfig = new HashMap<>();
+            this.specificConfig.put(Names.DEFAULT, new CfgDbDetails());
+        }
     }
 
     public void fromXML(final XMLStreamReader sr) throws XMLStreamException {
-        CfgDbDetails dbDefault = specificConfig.get(Names.DEFAULT);
-
         loop:
         while (sr.hasNext()) {
             int eventType = sr.next();
@@ -82,97 +95,71 @@ public class CfgDb {
                         case "path":
                             this.path = Cfg.readValue(sr);
                             break;
-                        case "expert":
-                            this.expert = Boolean.parseBoolean(Cfg.readValue(sr));
-                            break;
+                        // parameter considered only when expert==false
                         case "vendor":
-                            dbDefault.vendor = Cfg.readValue(sr);
+                            this.vendor = Cfg.readValue(sr);
                             break;
-                        case Props.ENABLE_AUTO_COMMIT:
-                            dbDefault.enable_auto_commit = Boolean.parseBoolean(Cfg.readValue(sr));
-                            break;
-                        case Props.ENABLE_DB_CACHE:
-                            dbDefault.enable_db_cache = Boolean.parseBoolean(Cfg.readValue(sr));
-                            break;
+                        // parameter considered only when expert==false
                         case Props.ENABLE_DB_COMPRESSION:
-                            dbDefault.enable_db_compression = Boolean.parseBoolean(Cfg.readValue(sr));
+                            this.compression = Boolean.parseBoolean(Cfg.readValue(sr));
                             break;
-                        case Props.ENABLE_HEAP_CACHE:
-                            dbDefault.enable_heap_cache = Boolean.parseBoolean(Cfg.readValue(sr));
-                            break;
-                        case Props.MAX_HEAP_CACHE_SIZE:
-                            dbDefault.max_heap_cache_size = Cfg.readValue(sr);
-                            break;
-                        case Props.ENABLE_HEAP_CACHE_STATS:
-                            dbDefault.enable_heap_cache_stats = Boolean.parseBoolean(Cfg.readValue(sr));
-                            break;
-                        case Props.BLOCK_SIZE:
-                            dbDefault.block_size = CfgDbDetails
-                                    .parseFileSizeSafe(Cfg.readValue(sr), dbDefault.block_size);
-                            break;
-                        case Props.MAX_FD_ALLOC:
-                            int i = Integer.parseInt(Cfg.readValue(sr));
-                            dbDefault.max_fd_open_alloc = Math.max(CfgDbDetails.MIN_FD_OPEN_ALLOC, i);
-                            break;
-                        case Props.WRITE_BUFFER_SIZE:
-                            dbDefault.write_buffer_size = CfgDbDetails
-                                    .parseFileSizeSafe(Cfg.readValue(sr), dbDefault.write_buffer_size);
-                            break;
-                        case Props.READ_BUFFER_SIZE:
-                            dbDefault.read_buffer_size = CfgDbDetails
-                                    .parseFileSizeSafe(Cfg.readValue(sr), dbDefault.read_buffer_size);
-                            break;
-                        case Props.DB_CACHE_SIZE:
-                            dbDefault.cache_size = CfgDbDetails
-                                    .parseFileSizeSafe(Cfg.readValue(sr), dbDefault.cache_size);
-                            break;
+                        // parameter considered only when expert==true
                         case Names.DEFAULT: {
-                            dbDefault.fromXML(sr);
-                            this.specificConfig.put(Names.DEFAULT, dbDefault);
+                            CfgDbDetails dbConfig = this.specificConfig.get(Names.DEFAULT);
+                            dbConfig.fromXML(sr);
+                            this.specificConfig.put(Names.DEFAULT, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.BLOCK: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.BLOCK, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.INDEX: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.INDEX, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.DETAILS: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.DETAILS, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.STORAGE: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.STORAGE, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.STATE: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.STATE, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.TRANSACTION: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.TRANSACTION, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.TX_POOL: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
                             this.specificConfig.put(Names.TX_POOL, dbConfig);
                             break;
                         }
+                        // parameter considered only when expert==true
                         case Names.TX_CACHE: {
                             CfgDbDetails dbConfig = new CfgDbDetails();
                             dbConfig.fromXML(sr);
@@ -188,13 +175,6 @@ public class CfgDb {
                     break loop;
             }
         }
-
-        // if not expert user set the same default vendor for all databases
-        if (!expert) {
-            for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
-                entry.getValue().vendor = dbDefault.vendor;
-            }
-        }
     }
 
     public String toXML() {
@@ -208,24 +188,34 @@ public class CfgDb {
             xmlWriter.writeStartElement("db");
 
             xmlWriter.writeCharacters("\r\n\t\t");
+            xmlWriter.writeComment("Sets the physical location on disk where data will be stored.");
+            xmlWriter.writeCharacters("\r\n\t\t");
             xmlWriter.writeStartElement("path");
             xmlWriter.writeCharacters(this.getPath());
             xmlWriter.writeEndElement();
 
             if (!expert) {
                 xmlWriter.writeCharacters("\r\n\t\t");
+                xmlWriter.writeComment(
+                        "Database implementation used to store data; supported options: leveldb, h2, rocksdb.");
+                xmlWriter.writeCharacters("\r\n\t\t");
+                xmlWriter.writeComment("Caution: changing implementation requires re-syncing from genesis!");
+                xmlWriter.writeCharacters("\r\n\t\t");
                 xmlWriter.writeStartElement("vendor");
-                xmlWriter.writeCharacters(this.specificConfig.get(Names.DEFAULT).vendor);
+                xmlWriter.writeCharacters(this.vendor);
+                xmlWriter.writeEndElement();
+
+                xmlWriter.writeCharacters("\r\n\t\t");
+                xmlWriter.writeComment(
+                        "Boolean value. Enable/disable database compression to trade storage space for execution time.");
+                xmlWriter.writeCharacters("\r\n\t\t");
+                xmlWriter.writeStartElement(Props.ENABLE_DB_COMPRESSION);
+                xmlWriter.writeCharacters(String.valueOf(this.compression));
                 xmlWriter.writeEndElement();
             } else {
-                xmlWriter.writeCharacters("\r\n\t\t");
-                xmlWriter.writeStartElement("expert");
-                xmlWriter.writeCharacters(String.valueOf(expert));
-                xmlWriter.writeEndElement();
-            }
-
-            for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
-                entry.getValue().toXML(entry.getKey(), xmlWriter, expert);
+                for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
+                    entry.getValue().toXML(entry.getKey(), xmlWriter, expert);
+                }
             }
 
             xmlWriter.writeCharacters("\r\n\t");
@@ -247,18 +237,42 @@ public class CfgDb {
     }
 
     public Map<String, Properties> asProperties() {
-        Map<String, Properties> props = new HashMap<>();
+        Map<String, Properties> propSet = new HashMap<>();
 
-        for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
-            props.put(entry.getKey(), entry.getValue().asProperties());
+        if (expert) {
+            for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
+                propSet.put(entry.getKey(), entry.getValue().asProperties());
+            }
+        } else {
+            Properties props = new Properties();
+            props.setProperty(Props.DB_TYPE, this.vendor);
+            props.setProperty(Props.ENABLE_DB_COMPRESSION, String.valueOf(this.compression));
+
+            props.setProperty(Props.ENABLE_DB_CACHE, "true");
+            props.setProperty(Props.DB_CACHE_SIZE, String.valueOf(128 * (int) Utils.MEGA_BYTE));
+
+            props.setProperty(Props.ENABLE_AUTO_COMMIT, "true");
+            props.setProperty(Props.ENABLE_HEAP_CACHE, "false");
+            props.setProperty(Props.MAX_HEAP_CACHE_SIZE, "32");
+            props.setProperty(Props.ENABLE_HEAP_CACHE_STATS, "false");
+
+            props.setProperty(Props.MAX_FD_ALLOC, "1024");
+            props.setProperty(Props.BLOCK_SIZE, String.valueOf(16 * (int) Utils.MEGA_BYTE));
+            props.setProperty(Props.WRITE_BUFFER_SIZE, String.valueOf(64 * (int) Utils.MEGA_BYTE));
+            props.setProperty(Props.READ_BUFFER_SIZE, String.valueOf(64 * (int) Utils.MEGA_BYTE));
+
+            propSet.put(Names.DEFAULT, props);
         }
 
-        return props;
+        return propSet;
     }
 
     public void setHeapCacheEnabled(boolean value) {
-        for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
-            entry.getValue().enable_heap_cache = value;
+        // already disabled when expert==false
+        if (expert) {
+            for (Map.Entry<String, CfgDbDetails> entry : specificConfig.entrySet()) {
+                entry.getValue().enable_heap_cache = value;
+            }
         }
     }
 }
