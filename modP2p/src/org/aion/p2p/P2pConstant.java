1d6aeb3decec0e57a65b80d6c894e969fc546c68
aion-6368726973 <aion.6368726973@gmail.com>
2018-04-25 03:14:17 +0800

    1) increase threads to handle kernel process 2) reduce rate control max routes per second

--- modP2p/src/org/aion/p2p/P2pConstant.java
+++ modP2p/src/org/aion/p2p/P2pConstant.java
@@ -16,8 +16,8 @@ public class P2pConstant {
 
     SEND_BUFFER_SIZE = 8192 * 1024, //
 
-    // max p2p in package capped at 10.
-    READ_MAX_RATE = 10,
+    // max p2p in package capped at 1.
+    READ_MAX_RATE = 1,
 
     // max p2p in package capped for tx broadcast.
     READ_MAX_RATE_TXBC = 20,
