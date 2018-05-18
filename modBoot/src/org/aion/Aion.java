47faf1436f2234e3ebbad3d5c4ff04db45788d24
aionJoey <joey@nuco.io>
2018-05-18 11:37:46 -0400

    logger feature

--- modBoot/src/org/aion/Aion.java
+++ modBoot/src/org/aion/Aion.java
@@ -1,26 +1,27 @@
-/**
- * ***************************************************************************** Copyright (c)
- * 2017-2018 Aion foundation.
+/*******************************************************************************
+ * Copyright (c) 2017-2018 Aion foundation.
  *
- * <p>This file is part of the aion network project.
+ *     This file is part of the aion network project.
  *
- * <p>The aion network project is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or any later version.
+ *     The aion network project is free software: you can redistribute it
+ *     and/or modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
  *
- * <p>The aion network project is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE. See the GNU General Public License for more details.
+ *     The aion network project is distributed in the hope that it will
+ *     be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *     See the GNU General Public License for more details.
  *
- * <p>You should have received a copy of the GNU General Public License along with the aion network
- * project source files. If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU General Public License
+ *     along with the aion network project source files.
+ *     If not, see <https://www.gnu.org/licenses/>.
  *
- * <p>Contributors to the aion source files in decreasing order of code volume:
  *
- * <p>Aion foundation.
+ * Contributors:
+ *     Aion foundation.
  *
- * <p>****************************************************************************
- */
+ ******************************************************************************/
 package org.aion;
 
 import static org.aion.crypto.ECKeyFac.ECKeyType.ED25519;
@@ -87,7 +88,7 @@ public class Aion {
         Logger LOG = AionLoggerFactory.getLogger(LogEnum.GEN.toString());
 
         System.out.println(
-                "                     _____                  \n"
+                          "                     _____                  \n"
                         + "      .'.       |  .~     ~.  |..          |\n"
                         + "    .'   `.     | |         | |  ``..      |\n"
                         + "  .''''''''`.   | |         | |      ``..  |\n"
