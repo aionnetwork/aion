44e73a0eb19a458b72e8f71f24c2454df0066e74
aionick <nick@nuco.io>
2018-05-18 15:11:38 -0400

    sixth refactor: separate TaskClear

---
+++ modP2pImpl/src/org/aion/p2p/impl1/TaskClear2.java
@@ -0,0 +1,57 @@
+package org.aion.p2p.impl1;
+
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.aion.p2p.impl.comm.Node;
+import org.aion.p2p.impl.comm.NodeMgr;
+
+public class TaskClear2 implements Runnable {
+    private static final int PERIOD_CLEAR = 20000;
+    private static final int TIMEOUT_OUTBOUND_NODES = 20000;
+
+    private final P2pMgr mgr;
+    private final NodeMgr nodeMgr;
+    private AtomicBoolean start;
+
+    public TaskClear2(P2pMgr _mgr, NodeMgr _nodeMgr, AtomicBoolean _start) {
+        this.mgr = _mgr;
+        this.nodeMgr = _nodeMgr;
+        this.start = _start;
+    }
+
+    @Override
+    public void run() {
+        while (start.get()) {
+            try {
+                Thread.sleep(PERIOD_CLEAR);
+
+                nodeMgr.timeoutInbound(this.mgr);
+
+                Iterator outboundIt = nodeMgr.getOutboundNodes().keySet().iterator();
+                while (outboundIt.hasNext()) {
+
+                    Object obj = outboundIt.next();
+
+                    if (obj == null) continue;
+
+                    int nodeIdHash = (int) obj;
+                    Node node = nodeMgr.getOutboundNodes().get(nodeIdHash);
+
+                    if (node == null) continue;
+
+                    if (System.currentTimeMillis() - node.getTimestamp()
+                        > TIMEOUT_OUTBOUND_NODES) {
+                        this.mgr.closeSocket(
+                            node.getChannel(),
+                            "outbound-timeout node=" + node.getIdShort());
+                        outboundIt.remove();
+                    }
+                }
+
+                nodeMgr.timeoutActive(this.mgr);
+
+            } catch (Exception e) {
+            }
+        }
+    }
+}
--- modP2pImpl/src/org/aion/p2p/impl1/TaskConnectPeers2.java
+++ modP2pImpl/src/org/aion/p2p/impl1/TaskConnectPeers2.java
@@ -26,9 +26,15 @@ public class TaskConnectPeers2 implements Runnable {
     private Selector selector;
     private ReqHandshake1 cachedReqHandshake1;
 
-    public TaskConnectPeers2(P2pMgr _mgr, AtomicBoolean _start, boolean _showLog, NodeMgr _nodeMgr,
-        int _maxActiveNodes, Selector _selector, LinkedBlockingQueue<MsgOut> _sendMsgQue,
-        ReqHandshake1 _cachedReqHandshake1) {
+    public TaskConnectPeers2(
+            P2pMgr _mgr,
+            AtomicBoolean _start,
+            boolean _showLog,
+            NodeMgr _nodeMgr,
+            int _maxActiveNodes,
+            Selector _selector,
+            LinkedBlockingQueue<MsgOut> _sendMsgQue,
+            ReqHandshake1 _cachedReqHandshake1) {
 
         this.start = _start;
         this.showLog = _showLog;
@@ -51,7 +57,8 @@ public class TaskConnectPeers2 implements Runnable {
             }
 
             if (this.nodeMgr.activeNodesSize() >= this.maxActiveNodes) {
-                if (this.showLog) System.out.println("<p2p-tcp-connect-peer pass max-active-nodes>");
+                if (this.showLog)
+                    System.out.println("<p2p-tcp-connect-peer pass max-active-nodes>");
                 continue;
             }
 
@@ -72,26 +79,26 @@ public class TaskConnectPeers2 implements Runnable {
             }
             int nodeIdHash = node.getIdHash();
             if (!this.nodeMgr.getOutboundNodes().containsKey(nodeIdHash)
-                && !this.nodeMgr.hasActiveNode(nodeIdHash)) {
+                    && !this.nodeMgr.hasActiveNode(nodeIdHash)) {
                 int _port = node.getPort();
                 try {
                     SocketChannel channel = SocketChannel.open();
 
                     channel.socket()
-                        .connect(
-                            new InetSocketAddress(node.getIpStr(), _port),
-                            TIMEOUT_OUTBOUND_CONNECT);
+                            .connect(
+                                    new InetSocketAddress(node.getIpStr(), _port),
+                                    TIMEOUT_OUTBOUND_CONNECT);
                     this.mgr.configChannel(channel);
 
                     if (channel.finishConnect() && channel.isConnected()) {
 
                         if (this.showLog)
                             System.out.println(
-                                "<p2p success-connect node-id="
-                                    + node.getIdShort()
-                                    + " ip="
-                                    + node.getIpStr()
-                                    + ">");
+                                    "<p2p success-connect node-id="
+                                            + node.getIdShort()
+                                            + " ip="
+                                            + node.getIpStr()
+                                            + ">");
 
                         SelectionKey sk = channel.register(this.selector, SelectionKey.OP_READ);
                         ChannelBuffer rb = new ChannelBuffer(this.showLog);
@@ -105,38 +112,38 @@ public class TaskConnectPeers2 implements Runnable {
 
                         if (this.showLog)
                             System.out.println(
-                                "<p2p prepare-request-handshake -> id="
-                                    + node.getIdShort()
-                                    + " ip="
-                                    + node.getIpStr()
-                                    + ">");
+                                    "<p2p prepare-request-handshake -> id="
+                                            + node.getIdShort()
+                                            + " ip="
+                                            + node.getIpStr()
+                                            + ">");
                         this.sendMsgQue.offer(
-                            new MsgOut(
-                                node.getIdHash(),
-                                node.getIdShort(),
-                                this.cachedReqHandshake1,
-                                Dest.OUTBOUND));
+                                new MsgOut(
+                                        node.getIdHash(),
+                                        node.getIdShort(),
+                                        this.cachedReqHandshake1,
+                                        Dest.OUTBOUND));
                         // node.peerMetric.decFailedCount();
 
                     } else {
                         if (this.showLog)
                             System.out.println(
-                                "<p2p fail-connect node-id="
-                                    + node.getIdShort()
-                                    + " ip="
-                                    + node.getIpStr()
-                                    + ">");
+                                    "<p2p fail-connect node-id="
+                                            + node.getIdShort()
+                                            + " ip="
+                                            + node.getIpStr()
+                                            + ">");
                         channel.close();
                         // node.peerMetric.incFailedCount();
                     }
                 } catch (IOException e) {
                     if (this.showLog)
                         System.out.println(
-                            "<p2p connect-outbound io-exception addr="
-                                + node.getIpStr()
-                                + ":"
-                                + _port
-                                + " result=failed>");
+                                "<p2p connect-outbound io-exception addr="
+                                        + node.getIpStr()
+                                        + ":"
+                                        + _port
+                                        + " result=failed>");
                     // node.peerMetric.incFailedCount();
                 } catch (Exception e) {
                     if (this.showLog) e.printStackTrace();
