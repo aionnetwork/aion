9bbe764b65c92129a73adb43913f11554e891cd7
jayTseng <jay.tseng@aion.network>
2018-07-27 14:34:30 -0400

    add 3rdParty lib build from source - libnsc

---
+++ 3rdParty/libnsc/prov/src/main/java/org/spongycastle/jce/interfaces/ElGamalPublicKey.java
@@ -0,0 +1,11 @@
+package org.spongycastle.jce.interfaces;
+
+import java.math.BigInteger;
+
+import javax.crypto.interfaces.DHPublicKey;
+
+public interface ElGamalPublicKey
+    extends ElGamalKey, DHPublicKey
+{
+    public BigInteger getY();
+}
