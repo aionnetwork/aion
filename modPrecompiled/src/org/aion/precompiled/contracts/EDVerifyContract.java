afecacb0926620e192f7f70a402b6fde965f1b8e
Alexandru Laurus <alexandru.laurus@gmail.com>
2018-08-02 14:50:13 +0300

    Add more test cases and document precompiled contract

--- modPrecompiled/src/org/aion/precompiled/contracts/EDVerifyContract.java
+++ modPrecompiled/src/org/aion/precompiled/contracts/EDVerifyContract.java
@@ -7,8 +7,6 @@ import org.aion.crypto.ed25519.Ed25519Signature;
 import org.aion.vm.ExecutionResult;
 import org.aion.vm.IPrecompiledContract;
 
-import java.util.Arrays;
-
 /**
  * Precompiled contract that verifies a signed message with the Ed25519 signature algorithm
  */
@@ -17,32 +15,36 @@ public class EDVerifyContract implements IPrecompiledContract {
     private final static long COST = 21000L;
 
     /**
-     * <p>
+     * TODO: may want return the actual public key/address
+     *
      * Method takes as input a byte array representing the edverify method parameters represented in hexadecimal
      * from the solidity binary as follows
-     * <p>
-     * edverify(bytes32 hash, bytes32 public_key, bytes signature)
-     * <p>
-     * input
-     * |
-     * | message hash (32 bytes) | public key (32 bytes) | signature bytes (64 bytes) |
-     * <p>
-     * where
-     * - message hash = keccak 256 hashed message
-     * - public key = public key of the ED25519 key pair
-     * - signature = 64 bytes of the ED25519 signature
      *
-     * @param input    The input arguments for the contract.
+     * edverify(bytes32 hash, bytes32 signature_part_1, bytes32 signature_part_2, bytes32 public_key)
+     *
+     *                                              input
+     *                                                |
+     * | message hash (32 bytes) | signature part 1 (32 bytes) | signature part 2 (32 bytes) | public key (32 bytes) |
+     *
+     * where
+     *      - message hash = keccak hashed message
+     *      - signature part 1 = first 32 bytes of the ED25519 signature bytes
+     *      - signature part 2 = last 32 bytes of the ED25519 signature bytes
+     *      - public key = public key of the ED25519 key pair
+     *
+     * @param input The input arguments for the contract.
      * @param nrgLimit The energy limit.
-     * @return byte array of length 32 containing: the address of the account that signed the message  if the message was
-     *                                              signed with the public key of the account sent as a parameter
-     * <p>
-     *                                              ZERO_ADDRESS 32 byte array of 0 otherwise
+     * @return byte array of length 1 with the byte set to: 1 if the message was signed with the public key of the account
+     * identified with the public key,
+     *                                                      0 otherwise
      */
     @Override
     public IExecutionResult execute(byte[] input, long nrgLimit) {
+        byte[] result = new byte[1];
+
         if (COST > nrgLimit) {
-            return new ExecutionResult(ExecutionResult.ResultCode.OUT_OF_NRG, 0, Address.ZERO_ADDRESS().toBytes());
+            result[0] = (byte) 0;
+            return new ExecutionResult(ExecutionResult.ResultCode.OUT_OF_NRG, 0, result);
         }
         byte[] msg = new byte[32];
         byte[] pubKey = new byte[32];
@@ -50,25 +52,21 @@ public class EDVerifyContract implements IPrecompiledContract {
 
         try {
             System.arraycopy(input, 0, msg, 0, 32);
-            System.arraycopy(input, 32, pubKey, 0, 32);
-            System.arraycopy(input, 64, sig, 0, 64);
+            System.arraycopy(input, 32, sig, 0, 64);
+            System.arraycopy(input, 96, pubKey, 0, 32);
         } catch (Exception e) {
-            //TODO: Logging ?
             System.out.println("Invalid input. Check if size is correct: " + e.getMessage());
-            return new ExecutionResult(ExecutionResult.ResultCode.INTERNAL_ERROR, 0, Address.ZERO_ADDRESS().toBytes());
+            result[0] = (byte) 0;
+            return new ExecutionResult(ExecutionResult.ResultCode.INTERNAL_ERROR, 0, result);
         }
 
         try {
             boolean verify = ECKeyEd25519.verify(msg, sig, pubKey);
-            if (verify) {
-                Ed25519Signature signature = new Ed25519Signature(pubKey, sig);
-                byte[] result = Arrays.copyOf(signature.getAddress(), Address.ADDRESS_LEN);
-                return new ExecutionResult(ExecutionResult.ResultCode.SUCCESS, nrgLimit - COST, result);
-            } else {
-                return new ExecutionResult(ExecutionResult.ResultCode.SUCCESS, nrgLimit - COST, Address.ZERO_ADDRESS().toBytes());
-            }
+            result[0] = verify ? (byte) 1 : (byte) 0;
+            return new ExecutionResult(ExecutionResult.ResultCode.SUCCESS, nrgLimit - COST, result);
         } catch (Throwable e) {
-            return new ExecutionResult(ExecutionResult.ResultCode.FAILURE, 0, Address.ZERO_ADDRESS().toBytes());
+            result[0] = (byte) 0;
+            return new ExecutionResult(ExecutionResult.ResultCode.FAILURE, 0, result);
         }
     }
 }
