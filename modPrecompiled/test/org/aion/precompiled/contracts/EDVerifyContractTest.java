e5349a1c6631228856b7cb3e8aa155659d1466b0
Alexandru Laurus <alexandru.laurus@gmail.com>
2018-08-02 14:50:13 +0300

    Add more test cases and document precompiled contract

--- modPrecompiled/test/org/aion/precompiled/contracts/EDVerifyContractTest.java
+++ modPrecompiled/test/org/aion/precompiled/contracts/EDVerifyContractTest.java
@@ -55,8 +55,31 @@ public class EDVerifyContractTest {
 
     @Test
     public void shouldReturnSuccessAnd1IfTheSignatureIsValid() {
+<<<<<<< HEAD
         byte[] input = setupInput(INPUT_BUFFER_LENGTH);
 
+=======
+        ECKeyFac.setType(ECKeyFac.ECKeyType.ED25519);
+        ECKey ecKey = ECKeyFac.inst().create();
+        ecKey = ecKey.fromPrivate(Hex.decode("5a90d8e67da5d1dfbf17916ae83bae04ef334f53ce8763932eba2c1116a62426fff4317ae351bda5e4fa24352904a9366d3a89e38d1ffa51498ba9acfbc65724"));
+
+        byte[] pubKey = ecKey.getPubKey();
+        String rawMessage = "This is a message from outer space";
+        String message = "\u0019Aion Signed Message:\n" + rawMessage.length() + rawMessage;
+        byte[] hashedMessage = HashUtil.keccak256(message.getBytes());
+
+        ISignature signature = ecKey.sign(hashedMessage);
+
+        byte[] input = new byte[128];
+        System.arraycopy(hashedMessage, 0, input, 0, 32);
+        System.arraycopy(signature.getSignature(), 0, input, 32, 64);
+        System.arraycopy(pubKey, 0, input, 96, 32);
+
+        ExecutionContext ctx = new ExecutionContext(txHash, ContractFactory.getEdVerifyContractAddress(), origin, caller, nrgPrice,
+                nrgLimit, callValue,
+                callData, depth, kind, flags, blockCoinbase, blockNumber, blockTimestamp, blockNrgLimit,
+                blockDifficulty);
+>>>>>>> 2147fee0... Add more test cases and document precompiled contract
         IPrecompiledContract contract = ContractFactory.getPrecompiledContract(ctx, null);
 
         IExecutionResult result = contract.execute(input, VALID_NRG_LIMIT);
@@ -65,6 +88,7 @@ public class EDVerifyContractTest {
 
     @Test
     public void shouldReturnSuccessAnd0IfSignatureIsNotValid() {
+<<<<<<< HEAD
         byte[] input = setupInput(INPUT_BUFFER_LENGTH);
         input[33] = 0;
 
@@ -80,6 +104,77 @@ public class EDVerifyContractTest {
         byte[] input = setupInput(127);
 
         IPrecompiledContract contract = ContractFactory.getPrecompiledContract(ctx, null);
+=======
+        ECKeyFac.setType(ECKeyFac.ECKeyType.ED25519);
+        ECKey ecKey = ECKeyFac.inst().create();
+        ecKey = ecKey.fromPrivate(Hex.decode("5a90d8e67da5d1dfbf17916ae83bae04ef334f53ce8763932eba2c1116a62426fff4317ae351bda5e4fa24352904a9366d3a89e38d1ffa51498ba9acfbc65724"));
+
+        byte[] pubKey = ecKey.getPubKey();
+        String rawMessage = "This is a message from outer space";
+        String message = "\u0019Aion Signed Message:\n" + rawMessage.length() + rawMessage;
+        byte[] hashedMessage = HashUtil.keccak256(message.getBytes());
+
+        ISignature signature = ecKey.sign(hashedMessage);
+
+        byte[] input = new byte[128];
+        System.arraycopy(hashedMessage, 0, input, 0, 32);
+        byte[] alteredSig = signature.getSignature();
+        alteredSig[0] = 1;
+        System.arraycopy(alteredSig, 0, input, 32, 64);
+        System.arraycopy(pubKey, 0, input, 96, 32);
+
+        ExecutionContext ctx = new ExecutionContext(txHash, ContractFactory.getEdVerifyContractAddress(), origin, caller, nrgPrice,
+                nrgLimit, callValue,
+                callData, depth, kind, flags, blockCoinbase, blockNumber, blockTimestamp, blockNrgLimit,
+                blockDifficulty);
+        IPrecompiledContract contract = ContractFactory.getPrecompiledContract(ctx, null);
+
+        IExecutionResult result = contract.execute(input, 21000L);
+        assertThat(result.getOutput()[0]).isEqualTo(0);
+        assertThat(result.getCode()).isEqualTo(ExecutionResult.ResultCode.SUCCESS.toInt());
+    }
+
+    @Test
+    public void shouldFailureAnd0IfInputIsNotValid() {
+        ECKeyFac.setType(ECKeyFac.ECKeyType.ED25519);
+        ECKey ecKey = ECKeyFac.inst().create();
+        ecKey = ecKey.fromPrivate(Hex.decode("5a90d8e67da5d1dfbf17916ae83bae04ef334f53ce8763932eba2c1116a62426fff4317ae351bda5e4fa24352904a9366d3a89e38d1ffa51498ba9acfbc65724"));
+
+        byte[] pubKey = ecKey.getPubKey();
+        String rawMessage = "This is a message from outer space";
+        String message = "\u0019Aion Signed Message:\n" + rawMessage.length() + rawMessage;
+        byte[] hashedMessage = HashUtil.keccak256(message.getBytes());
+
+        ISignature signature = ecKey.sign(hashedMessage);
+
+        byte[] input = new byte[127];
+        System.arraycopy(hashedMessage, 0, input, 0, 32);
+        System.arraycopy(signature.getSignature(), 0, input, 32, 63);
+        System.arraycopy(pubKey, 0, input, 95, 32);
+
+        ExecutionContext ctx = new ExecutionContext(txHash, ContractFactory.getEdVerifyContractAddress(), origin, caller, nrgPrice,
+                nrgLimit, callValue,
+                callData, depth, kind, flags, blockCoinbase, blockNumber, blockTimestamp, blockNrgLimit,
+                blockDifficulty);
+        IPrecompiledContract contract = ContractFactory.getPrecompiledContract(ctx, null);
+
+        IExecutionResult result = contract.execute(input, 21000L);
+        assertThat(result.getCode()).isEqualTo(ExecutionResult.ResultCode.INTERNAL_ERROR.toInt());
+        assertThat(result.getOutput()[0]).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnOutOfEnergyAnd0IfNotEnoughEnergy() {
+        nrgPrice = DataWord.ONE;
+        ECKeyFac.setType(ECKeyFac.ECKeyType.ED25519);
+        ECKey ecKey = ECKeyFac.inst().create();
+        ecKey = ecKey.fromPrivate(Hex.decode("5a90d8e67da5d1dfbf17916ae83bae04ef334f53ce8763932eba2c1116a62426fff4317ae351bda5e4fa24352904a9366d3a89e38d1ffa51498ba9acfbc65724"));
+
+        byte[] pubKey = ecKey.getPubKey();
+        String rawMessage = "This is a message from outer space";
+        String message = "\u0019Aion Signed Message:\n" + rawMessage.length() + rawMessage;
+        byte[] hashedMessage = HashUtil.keccak256(message.getBytes());
+>>>>>>> 2147fee0... Add more test cases and document precompiled contract
 
         IExecutionResult result = contract.execute(input, VALID_NRG_LIMIT);
         assertThat(result.getCode()).isEqualTo(ExecutionResult.ResultCode.INTERNAL_ERROR.toInt());
@@ -112,6 +207,7 @@ public class EDVerifyContractTest {
                 callData, depth, kind, flags, blockCoinbase, blockNumber, blockTimestamp, blockNrgLimit,
                 blockDifficulty);
 
+<<<<<<< HEAD
         ecKey = ECKeyFac.inst().create();
         ecKey = ecKey.fromPrivate(Hex.decode("5a90d8e67da5d1dfbf17916ae83bae04ef334f53ce8763932eba2c1116a62426f" +
                 "ff4317ae351bda5e4fa24352904a9366d3a89e38d1ffa51498ba9acfbc65724"));
@@ -120,6 +216,11 @@ public class EDVerifyContractTest {
         String message = "\u0019Aion Signed Message:\n" + rawMessage.length() + rawMessage;
         hashedMessage = HashUtil.keccak256(message.getBytes());
         signature = ecKey.sign(hashedMessage);
+=======
+        IExecutionResult result = contract.execute(input, 10000L);
+        assertThat(result.getCode()).isEqualTo(ExecutionResult.ResultCode.OUT_OF_NRG.toInt());
+        assertThat(result.getOutput()[0]).isEqualTo(0);
+>>>>>>> 2147fee0... Add more test cases and document precompiled contract
     }
 
     private byte[] setupInput(int length) {
