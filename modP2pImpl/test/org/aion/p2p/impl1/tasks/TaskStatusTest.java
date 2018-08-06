fa113f9cefd3c708c984b69fcf81d755d1450340
AionJayT <jay.tseng@aion.network>
2018-08-06 13:50:43 -0400

    add p2p taskStatus testcase

---
+++ modP2pImpl/test/org/aion/p2p/impl1/tasks/TaskStatusTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2017-2018 Aion foundation.
+ *      This file is part of the aion network project.
+ *
+ *      The aion network project is free software: you can redistribute it
+ *      and/or modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation, either version 3 of
+ *      the License, or any later version.
+ *
+ *      The aion network project is distributed in the hope that it will
+ *      be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *      See the GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the aion network project source files.
+ *      If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Contributors:
+ *      Aion foundation.
+ */
+
+package org.aion.p2p.impl1.tasks;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.aion.log.AionLoggerFactory;
+import org.aion.log.LogEnum;
+import org.aion.log.LogLevels;
+import org.aion.p2p.Handler;
+import org.aion.p2p.INodeMgr;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class TaskStatusTest {
+
+    @Mock
+    private BlockingQueue<MsgOut> msgOutQue;
+
+    @Mock
+    private BlockingQueue<MsgIn> msgInQue;
+
+    @Mock
+    private INodeMgr nodeMgr;
+
+
+    @Before
+    public void Setup() {
+        MockitoAnnotations.initMocks(this);
+
+        Map<String, String> logMap = new HashMap<>();
+        logMap.put(LogEnum.P2P.name(),  LogLevels.DEBUG.name());
+        AionLoggerFactory.init(logMap);
+    }
+
+
+    @Test
+    public void testRun() throws InterruptedException {
+
+        TaskStatus ts = new TaskStatus(nodeMgr, "1", msgOutQue, msgInQue);
+
+        when(nodeMgr.dumpNodeInfo(anyString(), anyBoolean())).thenReturn("get Status");
+
+        Thread t = new Thread(ts);
+        t.start();
+        assertTrue(t.isAlive());
+
+        Thread.sleep(100);
+        assertEquals("TERMINATED", t.getState().toString());
+    }
+
+
+}
