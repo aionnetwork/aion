53398fd1ca76c8eb4ec643ee8ffbe60631d61386
aion-kelvin <kelvin@aion.network>
2018-08-07 17:59:15 -0400

    trying to make accounts pane not go insane when maximizing

--- modGui/src/org/aion/wallet/ui/components/account/AccountCellItem.java
+++ modGui/src/org/aion/wallet/ui/components/account/AccountCellItem.java
@@ -4,6 +4,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
