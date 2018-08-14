49b6c45f154ade172e63c4ae40bca6fb54d312b3
joey <joey@nuco.io>
2018-08-14 16:52:08 -0400

    added license header, fixed ant dependency

--- modAionBase/test/org/aion/base/util/ByteUtilTest.java
+++ modAionBase/test/org/aion/base/util/ByteUtilTest.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2017-2018 Aion foundation.
+ *
+ *     This file is part of the aion network project.
+ *
+ *     The aion network project is free software: you can redistribute it
+ *     and/or modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
+ *
+ *     The aion network project is distributed in the hope that it will
+ *     be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *     See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with the aion network project source files.
+ *     If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Aion foundation.
+ */
+
 package org.aion.base.util;
 
 import junitparams.JUnitParamsRunner;
@@ -5,12 +28,10 @@ import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.sound.midi.SysexMessage;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 import static org.aion.base.util.ByteUtil.*;
 import static org.junit.Assert.*;
