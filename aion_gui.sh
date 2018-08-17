f054a1fcf3cd6144a049de2302e2fd5421a52f69
aion-kelvin <kelvin@aion.network>
2018-08-17 16:26:11 -0400

    aion_gui.sh needs to use the java in rt if available

--- aion_gui.sh
+++ aion_gui.sh
@@ -1,7 +1,15 @@
 #!/bin/bash 
+JAVA_CMD=java
+if [ -d "./rt" ]; then
+        JAVA_CMD="./rt/bin/java"
+elif [ -d "./pack/rt" ]; then
+        JAVA_CMD="./pack/rt/bin/java"
+elif [ -d "$JAVA_HOME" ]; then
+        JAVA_CMD="$JAVA_HOME/bin/java"
+fi
 
 STORAGE_DIR=${HOME}/.aion
-env EVMJIT="-cache=1" java -Xms4g \
+env EVMJIT="-cache=1" $JAVA_CMD -Xms4g \
     --add-opens=javafx.graphics/javafx.scene.text=ALL-UNNAMED \
     --add-opens=javafx.graphics/com.sun.javafx.text=ALL-UNNAMED \
     --add-opens=java.base/java.nio=ALL-UNNAMED \
