b34fcca650531e6cda840cd3868d413a32fc9b3a
Ali Sharif <ali@nuco.io>
2018-08-27 13:12:36 -0400

    rpc server reliability upgrades

--- modApiServer/src/org/aion/api/server/http/undertow/UndertowRpcServer.java
+++ modApiServer/src/org/aion/api/server/http/undertow/UndertowRpcServer.java
@@ -4,16 +4,14 @@ import io.undertow.Undertow;
 import io.undertow.io.Receiver;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.server.handlers.BlockingHandler;
-import io.undertow.server.handlers.RequestBufferingHandler;
-import io.undertow.server.handlers.RequestDumpingHandler;
-import io.undertow.server.handlers.StuckThreadDetectionHandler;
+import io.undertow.server.handlers.*;
 import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
 import io.undertow.util.Methods;
 import io.undertow.util.StatusCodes;
 import org.aion.api.server.http.RpcServer;
 import org.aion.api.server.http.RpcServerBuilder;
+import org.aion.api.server.rpc.RpcProcessor;
 import org.aion.log.AionLoggerFactory;
 import org.aion.log.LogEnum;
 import org.slf4j.Logger;
@@ -24,18 +22,20 @@ import javax.net.ssl.TrustManagerFactory;
 import java.io.FileInputStream;
 import java.security.KeyStore;
 import java.util.Map;
+import java.util.Optional;
 
 public class UndertowRpcServer extends RpcServer {
+
     private static final Logger LOG = AionLoggerFactory.getLogger(LogEnum.API.name());
+    private static final int STUCK_THREAD_TIMEOUT_SECONDS = 600; // 10 min
 
     Undertow server;
 
-    private final int STUCK_THREAD_TIMEOUT_SECONDS = 600; // 10 min
     private final Map<HttpString, String> CORS_HEADERS = Map.of(
             HttpString.tryFromString("Access-Control-Allow-Origin"), corsOrigin,
             HttpString.tryFromString("Access-Control-Allow-Headers"), "origin,accept,content-type",
             HttpString.tryFromString("Access-Control-Allow-Credentials"), "true",
-            HttpString.tryFromString("Access-Control-Allow-Methods"), "POST",
+            HttpString.tryFromString("Access-Control-Allow-Methods"), "POST,OPTIONS",
             HttpString.tryFromString("Access-Control-Max-Age"), "86400"
     );
 
@@ -55,59 +55,6 @@ public class UndertowRpcServer extends RpcServer {
         // hook up all the possible loggers defined (now and in future) by this library
         // through our logback logger; this risks missing potentially important debug information from the library
         System.setProperty("org.jboss.logging.provider", "slf4j");
-    }
-
-    private void addCorsHeaders(HttpServerExchange exchange) {
-        if (corsEnabled) {
-            for (Map.Entry<HttpString, String> header: CORS_HEADERS.entrySet()) {
-                exchange.getResponseHeaders().put(header.getKey(), header.getValue());
-            }
-        }
-    }
-
-    private void handleRequest(HttpServerExchange ex0) throws Exception {
-        Receiver.FullStringCallback rpcHandler = (ex3, body) -> {
-            // only support post requests
-            if (!Methods.POST.equals(ex3.getRequestMethod())) {
-                ex3.setStatusCode(StatusCodes.METHOD_NOT_ALLOWED);
-                ex3.endExchange();
-                return;
-            }
-
-            ex3.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-            addCorsHeaders(ex3);
-            ex3.getResponseSender().send(rpcProcessor.process(body));
-        };
-        HttpHandler corsPreflightHandler = ex1 -> {
-            if (corsEnabled && Methods.OPTIONS.equals(ex1.getRequestMethod())) {
-                ex1.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                ex1.getResponseHeaders().put(Headers.CONTENT_LENGTH, "0");
-                addCorsHeaders(ex1);
-                ex1.getResponseSender().send("");
-            } else {
-                new RequestBufferingHandler(ex2 -> ex2.getRequestReceiver().receiveFullString(rpcHandler), 10)
-                        .handleRequest(ex1);
-            }
-        };
-        /**
-         * Opinion: StuckThreadDetectionHandler should be enabled by default, since in the grand-scheme of things, it's
-         * performance overhead is not too great and it could potentially help us catch implementation bugs in the API.
-         * Alternative: Allow the user to enable this from the config?
-         * See Impl:
-         * github.com/undertow-io/undertow/blob/master/core/src/main/java/io/undertow/server/handlers/StuckThreadDetectionHandler.java
-         */
-        StuckThreadDetectionHandler stuckThreadDetectionHandler = new StuckThreadDetectionHandler(STUCK_THREAD_TIMEOUT_SECONDS, corsPreflightHandler);
-
-        // Only enabled if API is in TRACE mode
-        RequestDumpingHandler requestDumpingHandler = new RequestDumpingHandler(stuckThreadDetectionHandler);
-
-        HttpHandler firstHandler;
-        if (LOG.isTraceEnabled()) firstHandler = requestDumpingHandler;
-        else firstHandler = stuckThreadDetectionHandler;
-
-        BlockingHandler blockingHandler = new BlockingHandler(firstHandler);
-
-        blockingHandler.handleRequest(ex0);
     }
 
     private SSLContext sslContext() throws Exception {
@@ -145,17 +92,68 @@ public class UndertowRpcServer extends RpcServer {
             else
                 undertowBuilder.addHttpListener(port, hostName);
 
-            undertowBuilder.setHandler(this::handleRequest);
+            int effectiveIoThreadCount;
+
+            if (getIoPoolSize().isPresent()) {
+                LOG.info("<rpc-server - setting io thread count manually not recommended. recommended io thread pool size: {}>",
+                        Math.max(Runtime.getRuntime().availableProcessors(), 2));
+                undertowBuilder.setIoThreads(getIoPoolSize().get());
+
+                effectiveIoThreadCount = getIoPoolSize().get();
+            } else {
+                /** this number comes from {@link io.undertow.Undertow.Builder#Builder()} */
+                effectiveIoThreadCount = Math.max(Runtime.getRuntime().availableProcessors(), 2);
+            }
+
+            /** used to "remember" Undertow worker-thread count, since no getter exposed in {@link Undertow}. */
+            int effectiveWorkerThreadCount;
 
             if (getWorkerPoolSize().isPresent()) {
-                LOG.info("<rpc-server - setting worker thread count manually not recommended. recommended worker thread-pool size: {}>",
+                LOG.info("<rpc-server - setting worker thread count manually not recommended. recommended worker thread pool size: {}>",
                         Math.max(Runtime.getRuntime().availableProcessors(), 2) * 8);
                 undertowBuilder.setWorkerThreads(getWorkerPoolSize().get());
+
+                effectiveWorkerThreadCount = getWorkerPoolSize().get();
+            } else {
+                /** this number comes from {@link io.undertow.Undertow.Builder#Builder()} */
+                effectiveWorkerThreadCount = Math.max(Runtime.getRuntime().availableProcessors(), 2) * 8;
             }
+
+            StuckThreadDetectorConfiguration stuckThreadDetector =
+                    new StuckThreadDetectorConfiguration(false, STUCK_THREAD_TIMEOUT_SECONDS);
+            if (stuckThreadDetectorEnabled) {
+                stuckThreadDetector =
+                        new StuckThreadDetectorConfiguration(true, STUCK_THREAD_TIMEOUT_SECONDS);
+            }
+
+            RequestLimitingConfiguration requestLimiting =
+                    new RequestLimitingConfiguration(false, -1, 1);
+            boolean isQueueBounded = getRequestQueueSize().isPresent() && getRequestQueueSize().get() > 0;
+            if (isQueueBounded) {
+                requestLimiting = new RequestLimitingConfiguration(true, effectiveWorkerThreadCount,
+                        getRequestQueueSize().get());
+            }
+
+            AionUndertowRpcHandler rpcHandler = new AionUndertowRpcHandler(corsEnabled, CORS_HEADERS, rpcProcessor);
+
+            undertowBuilder.setHandler(new AionUndertowRootHandler(rpcHandler, requestLimiting, stuckThreadDetector));
+
+
             server = undertowBuilder.build();
             server.start();
 
             LOG.info("<rpc-server - (UNDERTOW) started on {}://{}:{}>", sslEnabled ? "https" : "http", hostName, port);
+
+            LOG.debug("----------------------------------------");
+            LOG.debug("UNDERTOW RPC Server Started with Options");
+            LOG.debug("----------------------------------------");
+            LOG.debug("SSL: {}", sslEnabled ? "Enabled; Certificate = "+sslCertCanonicalPath : "Not Enabled");
+            LOG.debug("CORS: {}", corsEnabled ? "Enabled; Allowed Origins = \""+corsOrigin+"\"" : "Not Enabled");
+            LOG.debug("Worker Thread Count: {}", effectiveWorkerThreadCount);
+            LOG.debug("I/O Thread Count: {}", effectiveIoThreadCount);
+            LOG.debug("Request Queue Size: {}", isQueueBounded ? getRequestQueueSize().get() : "Unbounded");
+            LOG.debug("----------------------------------------");
+
         } catch (Exception e) {
             LOG.error("<rpc-server - failed bind on {}:{}>", hostName, port);
             LOG.error("<rpc-server - " + e.getMessage() + ">");
