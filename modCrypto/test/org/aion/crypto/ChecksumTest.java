2698ac389ce7ab28ab341c0c34051eb26ad6493b
BeidouZ <beidouz@hotmail.com>
2018-09-25 14:48:25 -0400

    update test

--- modCrypto/test/org/aion/crypto/ChecksumTest.java
+++ modCrypto/test/org/aion/crypto/ChecksumTest.java
@@ -25,7 +25,6 @@ public class ChecksumTest {
     @Test
     public void testChecksum0x() {
         String input = "a0x896b9366f09e5efb1fa2ed9f3820b865ae97adbc6f364d691eb17784c9b1b"; //0x is not removed, h == null
-        String expected = "A08896B9366f09e5EfB1fA2ed9f3820B865AE97ADBc6f364D691eB17784c9b1b";
         assertEquals(Optional.empty(), (AddressSpecs.checksummedAddress(input)));
     }
 
