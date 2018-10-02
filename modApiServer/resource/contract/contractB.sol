fe9f7fb96850896373a9f76da78f36812c49b998
BeidouZ <beidouz@hotmail.com>
2018-10-02 18:16:17 -0400

    deleted old ones

--- modApiServer/resource/contract/contractB.sol
+++
@@ -1,7 +0,0 @@
-contract B{
-   event BE(address indexed _recipient, uint128 indexed _amount, bytes32 indexed _foreignNetworkId, bytes32 _foreignData);
-
-   function BB(address _recipient, uint128 _amount, bytes32 _foreignNetworkId, bytes32 _foreignData){
-        BE(_recipient, _amount, _foreignNetworkId, _foreignData);
-    }
-}
