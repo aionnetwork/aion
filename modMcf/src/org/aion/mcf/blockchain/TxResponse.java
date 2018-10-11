b354b0a76830395e4289bd2e23327a9bc679137c
Aayush Rajasekaran <arajasek94@gmail.com>
2018-10-11 11:52:51 -0400

    add val to enum

--- modMcf/src/org/aion/mcf/blockchain/TxResponse.java
+++ modMcf/src/org/aion/mcf/blockchain/TxResponse.java
@@ -1,18 +1,28 @@
 package org.aion.mcf.blockchain;
 
 public enum TxResponse {
-    SUCCESS(),
-    INVALID_TX(),
-    INVALID_TX_NRG_PRICE(),
-    INVALID_FROM(),
-    INVALID_ACCOUNT(),
-    ALREADY_CACHED(),
-    CACHED_NONCE(),
-    CACHED_POOLMAX(),
-    REPAID(),
-    ALREADY_SEALED(),
-    REPAYTX_POOL_EXCEPTION(),
-    REPAYTX_LOWPRICE(),
-    DROPPED(),
-    EXCEPTION(),
+    SUCCESS(0),
+    INVALID_TX(1),
+    INVALID_TX_NRG_PRICE(2),
+    INVALID_FROM(3),
+    INVALID_ACCOUNT(4),
+    ALREADY_CACHED(5),
+    CACHED_NONCE(6),
+    CACHED_POOLMAX(7),
+    REPAID(8),
+    ALREADY_SEALED(9),
+    REPAYTX_POOL_EXCEPTION(10),
+    REPAYTX_LOWPRICE(11),
+    DROPPED(12),
+    EXCEPTION(13);
+
+    private int val;
+
+    TxResponse(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
 }
