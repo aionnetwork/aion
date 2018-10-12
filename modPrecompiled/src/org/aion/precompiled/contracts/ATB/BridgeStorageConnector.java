d4ab60e5262d8f839f901be316e0990d6e7aaba3
BeidouZ <beidouz@hotmail.com>
2018-10-12 16:39:08 -0400

    modPrecompiled: license headers fixed

--- modPrecompiled/src/org/aion/precompiled/contracts/ATB/BridgeStorageConnector.java
+++ modPrecompiled/src/org/aion/precompiled/contracts/ATB/BridgeStorageConnector.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2017-2018 Aion foundation.
+ *
+ *     This file is part of the aion network project.
+ *
+ *     The aion network project is free software: you can redistribute it
+ *     and/or modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
+ *
+ *     The aion network project is distributed in the hope that it will
+ *     be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *     See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with the aion network project source files.
+ *     If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Aion foundation.
+ */
+
 package org.aion.precompiled.contracts.ATB;
 
 import java.math.BigInteger;
@@ -17,9 +40,8 @@ import org.aion.precompiled.PrecompiledUtilities;
 /**
  * Storage layout mapping as the following:
  *
- * ------------------------------------------------------------ owner            DWORD   0x0
- * current owner of the contract newOwner         DWORD   0x1     proposed new owner of the
- * contract
+ * ------------------------------------------------------------ owner            DWORD   0x0 current
+ * owner of the contract newOwner         DWORD   0x1     proposed new owner of the contract
  *
  * memberCount      WORD    0x2     the total amount of members minThresh        WORD    0x3     the
  * minimum amount of votes required ringLocked       WORD    0x4     if the ring is locked or not
