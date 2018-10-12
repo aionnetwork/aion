5bdac502283b9b0382402627c42750b09174e18c
BeidouZ <beidouz@hotmail.com>
2018-10-12 15:46:29 -0400

    modLogger: reformat & licence header update

--- modLogger/src/org/aion/log/LogUtil.java
+++ modLogger/src/org/aion/log/LogUtil.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2017-2018 Aion foundation.
  *
  *     This file is part of the aion network project.
@@ -19,25 +19,31 @@
  *
  * Contributors:
  *     Aion foundation.
- *     
- ******************************************************************************/
+ */
 
 package org.aion.log;
 
 /**
  * Some logging utilities
- * 
- * @author yao
  *
+ * @author yao
  */
 public class LogUtil {
+
     /**
-     * Protected for now, until we can figure out why it crashes kernel, then we
-     * will integrate. See : <a href=
-     * 'http://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java'>stackoverflow
+     * Protected for now, until we can figure out why it crashes kernel, then we will integrate. See
+     * : <a href= 'http://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java'>stackoverflow
      * discussion</a> and our benchmark for performance gains
      */
     final protected static char[] hexArray = "0123456789abcfef".toCharArray();
+    /**
+     * Guarantees a return of at max, first 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
+     *
+     * @param data
+     * @return
+     */
+    protected static final String nullString = "";
 
     protected static String toHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
@@ -48,15 +54,6 @@ public class LogUtil {
         }
         return new String(hexChars);
     }
-
-    /**
-     * Guarantees a return of at max, first 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
-     * @param data
-     * @return
-     */
-    protected static final String nullString = "";
 
     protected static String toHexF8Internal(byte[] data) {
         if (data == null || data.length == 0) {
@@ -71,11 +68,8 @@ public class LogUtil {
     }
 
     /**
-     * Guarantees a return of at max, last 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
-     * @param data
-     * @return
+     * Guarantees a return of at max, last 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
      */
     public static String toHexL8Internal(byte[] data) {
         if (data == null || data.length == 0) {
@@ -90,31 +84,27 @@ public class LogUtil {
     }
 
     /**
-     * Guarantees a return of at max, first 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
-     * @param data
-     * @return
+     * Guarantees a return of at max, first 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
      */
     public static String toHexF8(byte[] data) {
         int len = 0;
-        if (data != null)
+        if (data != null) {
             len = data.length;
+        }
 
         return toHexF8Internal(data) + "... l=" + len;
     }
 
     /**
-     * Guarantees a return of at max, last 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
-     * @param data
-     * @return
+     * Guarantees a return of at max, last 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
      */
     public static String toHexL8(byte[] data) {
         int len = 0;
-        if (data != null)
+        if (data != null) {
             len = data.length;
+        }
 
         return "..." + toHexL8Internal(data) + " l=" + len;
     }
