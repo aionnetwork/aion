7cf2cbf332192c835fbe6cf5f2564dc407558163
BeidouZ <beidouz@hotmail.com>
2018-10-19 17:41:09 -0400

    update license headers in modVM

--- modVM/src/org/aion/vm/IPrecompiledContract.java
+++ modVM/src/org/aion/vm/IPrecompiledContract.java
@@ -1,23 +1,24 @@
 /*
  * Copyright (c) 2017-2018 Aion foundation.
- *      This file is part of the aion network project.
  *
- *      The aion network project is free software: you can redistribute it
- *      and/or modify it under the terms of the GNU General Public License
- *      as published by the Free Software Foundation, either version 3 of
- *      the License, or any later version.
+ *     This file is part of the aion network project.
  *
- *      The aion network project is distributed in the hope that it will
- *      be useful, but WITHOUT ANY WARRANTY; without even the implied
- *      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *      See the GNU General Public License for more details.
+ *     The aion network project is free software: you can redistribute it
+ *     and/or modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
  *
- *      You should have received a copy of the GNU General Public License
- *      along with the aion network project source files.
- *      If not, see <https://www.gnu.org/licenses/>.
+ *     The aion network project is distributed in the hope that it will
+ *     be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *     See the GNU General Public License for more details.
  *
- *  Contributors:
- *      Aion foundation.
+ *     You should have received a copy of the GNU General Public License
+ *     along with the aion network project source files.
+ *     If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Aion foundation.
  */
 package org.aion.vm;
 
