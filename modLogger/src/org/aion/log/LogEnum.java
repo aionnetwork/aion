9d7220992bb9e07a6f679a92310961b0467c386d
BeidouZ <beidouz@hotmail.com>
2018-10-19 14:44:34 -0400

    reformat modLogger

--- modLogger/src/org/aion/log/LogEnum.java
+++ modLogger/src/org/aion/log/LogEnum.java
@@ -1,39 +1,45 @@
-/*******************************************************************************
- * Copyright (c) 2017-2018 Aion foundation.
+/**
+ * ***************************************************************************** Copyright (c)
+ * 2017-2018 Aion foundation.
  *
- *     This file is part of the aion network project.
+ * <p>This file is part of the aion network project.
  *
- *     The aion network project is free software: you can redistribute it
- *     and/or modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation, either version 3 of
- *     the License, or any later version.
+ * <p>The aion network project is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
  *
- *     The aion network project is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied
- *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *     See the GNU General Public License for more details.
+ * <p>The aion network project is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with the aion network project source files.
- *     If not, see <https://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU General Public License along with the aion network
+ * project source files. If not, see <https://www.gnu.org/licenses/>.
  *
- * Contributors:
- *     Aion foundation.
- *     
- ******************************************************************************/
-
+ * <p>Contributors: Aion foundation.
+ *
+ * <p>****************************************************************************
+ */
 package org.aion.log;
 
-/**
- * Logger modules available to classes in the kernel
- */
+/** Logger modules available to classes in the kernel */
 public enum LogEnum {
-    GEN, CONS, SYNC, API, VM, NET, DB, EVTMGR, TXPOOL, TX, P2P, ROOT, GUI;
+    GEN,
+    CONS,
+    SYNC,
+    API,
+    VM,
+    NET,
+    DB,
+    EVTMGR,
+    TXPOOL,
+    TX,
+    P2P,
+    ROOT,
+    GUI;
 
     public static boolean contains(String _module) {
         for (LogEnum module : values()) {
-            if (module.name().equalsIgnoreCase(_module))
-                return true;
+            if (module.name().equalsIgnoreCase(_module)) return true;
         }
 
         return false;
--- modLogger/src/org/aion/log/LogUtil.java
+++ modLogger/src/org/aion/log/LogUtil.java
@@ -1,43 +1,39 @@
-/*******************************************************************************
- * Copyright (c) 2017-2018 Aion foundation.
+/**
+ * ***************************************************************************** Copyright (c)
+ * 2017-2018 Aion foundation.
  *
- *     This file is part of the aion network project.
+ * <p>This file is part of the aion network project.
  *
- *     The aion network project is free software: you can redistribute it
- *     and/or modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation, either version 3 of
- *     the License, or any later version.
+ * <p>The aion network project is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
  *
- *     The aion network project is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied
- *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *     See the GNU General Public License for more details.
+ * <p>The aion network project is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with the aion network project source files.
- *     If not, see <https://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU General Public License along with the aion network
+ * project source files. If not, see <https://www.gnu.org/licenses/>.
  *
- * Contributors:
- *     Aion foundation.
- *     
- ******************************************************************************/
-
+ * <p>Contributors: Aion foundation.
+ *
+ * <p>****************************************************************************
+ */
 package org.aion.log;
 
 /**
  * Some logging utilities
- * 
- * @author yao
  *
+ * @author yao
  */
 public class LogUtil {
     /**
-     * Protected for now, until we can figure out why it crashes kernel, then we
-     * will integrate. See : <a href=
+     * Protected for now, until we can figure out why it crashes kernel, then we will integrate. See
+     * : <a href=
      * 'http://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java'>stackoverflow
      * discussion</a> and our benchmark for performance gains
      */
-    final protected static char[] hexArray = "0123456789abcfef".toCharArray();
+    protected static final char[] hexArray = "0123456789abcfef".toCharArray();
 
     protected static String toHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
@@ -50,9 +46,9 @@ public class LogUtil {
     }
 
     /**
-     * Guarantees a return of at max, first 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
+     * Guarantees a return of at max, first 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
+     *
      * @param data
      * @return
      */
@@ -71,9 +67,9 @@ public class LogUtil {
     }
 
     /**
-     * Guarantees a return of at max, last 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
+     * Guarantees a return of at max, last 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
+     *
      * @param data
      * @return
      */
@@ -90,31 +86,29 @@ public class LogUtil {
     }
 
     /**
-     * Guarantees a return of at max, first 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
+     * Guarantees a return of at max, first 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
+     *
      * @param data
      * @return
      */
     public static String toHexF8(byte[] data) {
         int len = 0;
-        if (data != null)
-            len = data.length;
+        if (data != null) len = data.length;
 
         return toHexF8Internal(data) + "... l=" + len;
     }
 
     /**
-     * Guarantees a return of at max, last 8 characters, even if data is ill
-     * formatted (null, shorter, longer) etc.
-     * 
+     * Guarantees a return of at max, last 8 characters, even if data is ill formatted (null,
+     * shorter, longer) etc.
+     *
      * @param data
      * @return
      */
     public static String toHexL8(byte[] data) {
         int len = 0;
-        if (data != null)
-            len = data.length;
+        if (data != null) len = data.length;
 
         return "..." + toHexL8Internal(data) + " l=" + len;
     }
