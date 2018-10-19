c9c99686a4d611374f48ed270f9d0e38eb4c0dcd
BeidouZ <beidouz@hotmail.com>
2018-10-19 15:00:46 -0400

    reformat modTxPool

--- modTxPool/src/org/aion/txpool/ITxPool.java
+++ modTxPool/src/org/aion/txpool/ITxPool.java
@@ -1,35 +1,31 @@
-/*******************************************************************************
- * Copyright (c) 2017-2018 Aion foundation.
+/**
+ * ***************************************************************************** Copyright (c)
+ * 2017-2018 Aion foundation.
  *
- *     This file is part of the aion network project.
+ * <p>This file is part of the aion network project.
  *
- *     The aion network project is free software: you can redistribute it
- *     and/or modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation, either version 3 of
- *     the License, or any later version.
+ * <p>The aion network project is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
  *
- *     The aion network project is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied
- *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *     See the GNU General Public License for more details.
+ * <p>The aion network project is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with the aion network project source files.
- *     If not, see <https://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU General Public License along with the aion network
+ * project source files. If not, see <https://www.gnu.org/licenses/>.
  *
- * Contributors:
- *     Aion foundation.
+ * <p>Contributors: Aion foundation.
  *
- ******************************************************************************/
-
+ * <p>****************************************************************************
+ */
 package org.aion.txpool;
-
-import org.aion.base.type.Address;
-import org.aion.base.type.ITransaction;
 
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
+import org.aion.base.type.Address;
+import org.aion.base.type.ITransaction;
 
 /**
  * Aion pending state should be the only user of transaction pool.
@@ -45,7 +41,8 @@ public interface ITxPool<TX extends ITransaction> {
 
     List<TX> add(List<TX> tx);
 
-    // return TX if the TX add success, if the pool already has the same nonce tx. return the old tx.
+    // return TX if the TX add success, if the pool already has the same nonce tx. return the old
+    // tx.
     TX add(TX tx);
 
     List<TX> remove(List<TX> tx);
@@ -64,10 +61,10 @@ public interface ITxPool<TX extends ITransaction> {
 
     void updateBlkNrgLimit(long nrg);
 
-    @SuppressWarnings("SameReturnValue") String getVersion();
+    @SuppressWarnings("SameReturnValue")
+    String getVersion();
 
     List<TX> snapshotAll();
 
     TX getPoolTx(Address from, BigInteger txNonce);
-
 }
--- modTxPool/src/org/aion/txpool/TxPoolModule.java
+++ modTxPool/src/org/aion/txpool/TxPoolModule.java
@@ -1,32 +1,28 @@
-/*******************************************************************************
- * Copyright (c) 2017-2018 Aion foundation.
+/**
+ * ***************************************************************************** Copyright (c)
+ * 2017-2018 Aion foundation.
  *
- *     This file is part of the aion network project.
+ * <p>This file is part of the aion network project.
  *
- *     The aion network project is free software: you can redistribute it
- *     and/or modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation, either version 3 of
- *     the License, or any later version.
+ * <p>The aion network project is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
  *
- *     The aion network project is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied
- *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *     See the GNU General Public License for more details.
+ * <p>The aion network project is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with the aion network project source files.
- *     If not, see <https://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU General Public License along with the aion network
+ * project source files. If not, see <https://www.gnu.org/licenses/>.
  *
- * Contributors:
- *     Aion foundation.
- *     
- ******************************************************************************/
-
+ * <p>Contributors: Aion foundation.
+ *
+ * <p>****************************************************************************
+ */
 package org.aion.txpool;
 
-import org.aion.base.type.ITransaction;
-
 import java.util.Properties;
+import org.aion.base.type.ITransaction;
 
 public final class TxPoolModule {
     private static TxPoolModule singleton = null;
@@ -38,8 +34,13 @@ public final class TxPoolModule {
     private TxPoolModule(Properties config) throws Throwable {
         String moduleName = (String) config.get(MODULENAME);
         if (moduleName != null) {
-            TXPOOL = (ITxPool<ITransaction>) getClass().getClassLoader().loadClass(moduleName)
-                    .getDeclaredConstructor(Properties.class).newInstance(config);
+            TXPOOL =
+                    (ITxPool<ITransaction>)
+                            getClass()
+                                    .getClassLoader()
+                                    .loadClass(moduleName)
+                                    .getDeclaredConstructor(Properties.class)
+                                    .newInstance(config);
         } else {
             throw new Exception("No module name input!");
         }
