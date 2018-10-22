f5dc7a19317fa1573d185d3e1b8ea02654ef107d
Aayush Rajasekaran <arajasek94@gmail.com>
2018-10-22 17:35:05 -0400

    patch PendingStateTest formatting

--- modAionImpl/test/org/aion/zero/impl/PendingStateTest.java
+++ modAionImpl/test/org/aion/zero/impl/PendingStateTest.java
@@ -23,11 +23,12 @@
 
 package org.aion.zero.impl;
 
+import static org.junit.Assert.assertEquals;
+
 import java.math.BigInteger;
 import org.aion.base.type.Address;
 import org.aion.crypto.ECKey;
 import org.aion.mcf.blockchain.TxResponse;
-import static org.junit.Assert.*;
 import org.aion.zero.impl.config.CfgAion;
 import org.aion.zero.types.AionTransaction;
 import org.junit.Test;
@@ -37,10 +38,11 @@ public class PendingStateTest {
     @Test
     public void TestAddPendingTransactionSuccess() {
 
-        StandaloneBlockchain.Bundle bundle = new StandaloneBlockchain.Builder()
-            .withValidatorConfiguration("simple")
-            .withDefaultAccounts()
-            .build();
+        StandaloneBlockchain.Bundle bundle =
+                new StandaloneBlockchain.Builder()
+                        .withValidatorConfiguration("simple")
+                        .withDefaultAccounts()
+                        .build();
         StandaloneBlockchain bc = bundle.bc;
 
         CfgAion.inst().setGenesis(bc.getGenesis());
@@ -52,27 +54,28 @@ public class PendingStateTest {
 
         // Successful transaction
 
-        AionTransaction tx = new AionTransaction(
-            BigInteger.ZERO.toByteArray(),
-            to,
-            new byte[0],
-            new byte[0],
-            1_000_000L,
-            10_000_000_000L
-        );
+        AionTransaction tx =
+                new AionTransaction(
+                        BigInteger.ZERO.toByteArray(),
+                        to,
+                        new byte[0],
+                        new byte[0],
+                        1_000_000L,
+                        10_000_000_000L);
 
         tx.sign(signer);
 
-        assertEquals(hub.getPendingState().addPendingTransaction(tx),TxResponse.SUCCESS);
+        assertEquals(hub.getPendingState().addPendingTransaction(tx), TxResponse.SUCCESS);
     }
 
     @Test
     public void TestAddPendingTransactionInvalidNrgPrice() {
 
-        StandaloneBlockchain.Bundle bundle = new StandaloneBlockchain.Builder()
-            .withValidatorConfiguration("simple")
-            .withDefaultAccounts()
-            .build();
+        StandaloneBlockchain.Bundle bundle =
+                new StandaloneBlockchain.Builder()
+                        .withValidatorConfiguration("simple")
+                        .withDefaultAccounts()
+                        .build();
         StandaloneBlockchain bc = bundle.bc;
 
         CfgAion.inst().setGenesis(bc.getGenesis());
@@ -84,16 +87,17 @@ public class PendingStateTest {
 
         // Invalid Nrg Price transaction
 
-        AionTransaction tx = new AionTransaction(
-            BigInteger.ZERO.toByteArray(),
-            to,
-            new byte[0],
-            new byte[0],
-            1_000_000L,
-            1L
-        );
+        AionTransaction tx =
+                new AionTransaction(
+                        BigInteger.ZERO.toByteArray(),
+                        to,
+                        new byte[0],
+                        new byte[0],
+                        1_000_000L,
+                        1L);
         tx.sign(signer);
 
-        assertEquals(hub.getPendingState().addPendingTransaction(tx),TxResponse.INVALID_TX_NRG_PRICE);
+        assertEquals(
+                hub.getPendingState().addPendingTransaction(tx), TxResponse.INVALID_TX_NRG_PRICE);
     }
 }
