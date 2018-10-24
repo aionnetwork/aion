bcd51cb0381ddef3f762ffee4d230b6456496d2c
Timur Badretdinov <destinerx@gmail.com>
2018-10-24 14:45:56 +0300

    Remove assert from testCheckArguments

--- modAionImpl/test/org/aion/zero/impl/cli/CliTest.java
+++ modAionImpl/test/org/aion/zero/impl/cli/CliTest.java
@@ -1097,13 +1097,6 @@ public class CliTest {
         skippedTasks = cli.getSkippedTasks(options, breakingTaskPriority);
         assertTrue(skippedTasks.contains("--info"));
         assertTrue(skippedTasks.contains("-s create"));
-
-        args = new String[] {"--network", "--help"};
-        parser.parse(args);
-        breakingTaskPriority = cli.getBreakingTaskPriority(options);
-        assertEquals(0, breakingTaskPriority);
-        skippedTasks = cli.getSkippedTasks(options, breakingTaskPriority);
-        assertTrue(skippedTasks.contains("--network"));
     }
 
     // Methods below taken from FileUtils class
