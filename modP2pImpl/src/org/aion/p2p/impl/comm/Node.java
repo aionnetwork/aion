8cbb442c34f2ef49f01864ab05942879541339f3
Alexandra <alexandra@nuco.io>
2018-12-10 14:16:10 -0500

    decoupling p2p modules from modAionBase

--- modP2pImpl/src/org/aion/p2p/impl/comm/Node.java
+++ modP2pImpl/src/org/aion/p2p/impl/comm/Node.java
@@ -28,9 +28,9 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Arrays;
 import java.util.regex.Pattern;
-import org.aion.base.util.ByteUtil;
 import org.aion.p2p.INode;
 import org.aion.p2p.IPeerMetric;
+import org.aion.util.bytes.ByteUtil;
 
 /**
  * @author Chris p2p://{node-id}@{ip}:{port} node-id could be any non-empty string update to 36
