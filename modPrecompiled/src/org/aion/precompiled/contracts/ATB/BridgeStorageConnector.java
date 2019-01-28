1a3d58eedf8dc30e809e0a6f6940d4eef41c32be
aionick <nick@nuco.io>
2019-01-28 12:56:18 -0500

    Fix to bridge contract data alignment
    - The bridge contract was not re-aligning its storage bytes to be
      16 or 32 bytes long, which is now its responsibility. This is just
      a simple change to make that happen.

--- modPrecompiled/src/org/aion/precompiled/contracts/ATB/BridgeStorageConnector.java
+++ modPrecompiled/src/org/aion/precompiled/contracts/ATB/BridgeStorageConnector.java
@@ -215,7 +215,7 @@ public class BridgeStorageConnector {
         ByteArrayWrapper word = this.track.getStorageValue(contractAddress, key.toWrapper());
         // C1
         if (word == null || Arrays.equals(word.getData(), ByteUtil.EMPTY_HALFWORD)) return null;
-        return word.getData();
+        return alignBytes(word.getData());
     }
 
     private void setWORD(@Nonnull final DataWord key, @Nonnull final DataWord word) {
@@ -233,6 +233,16 @@ public class BridgeStorageConnector {
         if (word == null) return null;
 
         if (word.isZero()) return null;
-        return word.getData();
+        return alignBytes(word.getData());
+    }
+
+    private byte[] alignBytes(byte[] unalignedBytes) {
+        if (unalignedBytes == null) {
+            return null;
+        }
+
+        return (unalignedBytes.length > DataWord.BYTES)
+                ? new DoubleDataWord(unalignedBytes).getData()
+                : new DataWord(unalignedBytes).getData();
     }
 }
