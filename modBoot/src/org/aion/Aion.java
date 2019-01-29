0d85bbfd2909d3800e90d6747b07864add57c9f3
aionick <nick@nuco.io>
2019-01-29 14:14:20 -0500

    VMs init'd at start up, refactor VMFactory
    - The startup routine in Aion.java now initializes the virtual machines and
      adds their shutdown to the shutdown hook.
    - The VirtualMachineFactory has been replaced by a more flexible design with
      less reliance on static state.

--- modBoot/src/org/aion/Aion.java
+++ modBoot/src/org/aion/Aion.java
@@ -45,6 +45,7 @@ import org.aion.mcf.config.CfgSsl;
 import org.aion.mcf.mine.IMineRunner;
 import org.aion.solidity.Compiler;
 import org.aion.utils.NativeLibrary;
+import org.aion.vm.VirtualMachineProvider;
 import org.aion.zero.impl.blockchain.AionFactory;
 import org.aion.zero.impl.blockchain.IAionChain;
 import org.aion.zero.impl.cli.Cli;
@@ -240,6 +241,7 @@ public class Aion {
         /*
          * Start Threads.
          */
+        VirtualMachineProvider.initializeAllVirtualMachines();
         Thread zmqThread = null;
         ProtocolProcessor processor = null;
         if (cfg.getApi().getZmq().getActive()) {
@@ -377,6 +379,9 @@ public class Aion {
 
                                     genLog.info("Shutting down the AionHub...");
                                     ac.getAionHub().close();
+
+                                    genLog.info("Shutting down the virtual machines...");
+                                    VirtualMachineProvider.shutdownAllVirtualMachines();
 
                                     genLog.info("---------------------------------------------");
                                     genLog.info("| Aion kernel graceful shutdown successful! |");
