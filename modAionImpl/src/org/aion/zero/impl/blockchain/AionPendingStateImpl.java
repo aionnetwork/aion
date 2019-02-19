b6fb4a508f15590f01634158cece1f29c2db1158
aionWilliam <william@aion.network>
2019-02-19 14:12:01 -0500

    added more logging info for test harness usage

--- modAionImpl/src/org/aion/zero/impl/blockchain/AionPendingStateImpl.java
+++ modAionImpl/src/org/aion/zero/impl/blockchain/AionPendingStateImpl.java
@@ -714,8 +714,8 @@ public class AionPendingStateImpl implements IPendingStateInternal<AionBlock, Ai
         if (txSum.isRejected()) {
             if (LOGGER_TX.isTraceEnabled()) {
                 LOGGER_TX.trace(
-                        "addPendingTransactionImpl tx is rejected due to: {}",
-                        txSum.getReceipt().getError());
+                        "addPendingTransactionImpl tx " + Hex.toHexString(tx.getTransactionHash())
+                                + " is rejected due to: {}", txSum.getReceipt().getError());
             }
             fireTxUpdate(txSum.getReceipt(), PendingTransactionState.DROPPED, best.get());
             return TxResponse.DROPPED;
