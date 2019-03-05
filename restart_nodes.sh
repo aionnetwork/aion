2fea945e39ed1b935895eab9dae52399db15ffb4
aionick <nick@nuco.io>
2019-03-05 11:08:58 -0500

    Added seed node config files and other testnet scripts
    - Updated all config files to use new ID's.
    - Modified genesis file.
    - Added basic dapp.jar file.

---
+++ restart_nodes.sh
@@ -0,0 +1,54 @@
+nodes="138.91.123.106 23.100.52.181 13.91.127.35"
+avm_repo="../AVM/"
+
+aion_repo="$(pwd)" \
+&& echo "Checking out the latest version of kernel avmtestnet-03-05-2019 branch ..." \
+&& git checkout avmtestnet-03-05-2019 \
+&& git pull origin avmtestnet-03-05-2019 \
+&& git submodule update --init --recursive \
+&& cd $avm_repo \
+&& echo "Checking out latest version of Avm master branch ..." \
+&& git checkout master \
+&& git pull origin master \
+&& echo "Building latest Avm jar ..." \
+&& ant \
+&& cd "$aion_repo"/lib/ \
+&& rm org-aion-avm-*.jar \
+&& rm avm.jar \
+&& cp ../"$avm_repo"build/main/org-aion-avm-*.jar . \
+&& cp ../"$avm_repo"dist/avm.jar . \
+&& echo "Building the avm-integrated kernel release ..." \
+&& cd .. \
+&& ./gradlew clean \
+&& ./gradlew pack \
+&& cd pack
+
+# First clean all the nodes...
+echo " "
+echo "1. Cleaning all nodes ..."
+for node in $nodes
+do
+	echo "Cleaning node: $node ..." \
+	&& ssh aion@"$node" "./clean_node.sh"
+done
+
+echo " "
+echo "2. Updating all nodes with newest kernel build ..."
+for node in $nodes
+do
+	echo "Sending the newly built kernel release to node: $node ..." \
+	&& scp aion-v0.3.2*.tar.bz2 aion@"$node":/home/aion/
+done
+
+echo " "
+echo "3. Re-starting all nodes ..."
+number=1000
+for node in $nodes
+do
+	echo "Setting up the node and launching the kernel ..." \
+	&& ssh aion@"$node" "./start_node.sh "$number"" \
+	&& echo "Successfully cleaned and re-started node: $node!"
+	number=$((number + 1000))
+done
+
+
