947058210de5a4790114d6ce6811afd36d2412ee
AionJayT <jay.tseng@aion.network>
2019-03-13 16:08:20 -0400

    add commonts in VMException

--- modVM/src/org/aion/vm/exception/VMException.java
+++ modVM/src/org/aion/vm/exception/VMException.java
@@ -1,7 +1,14 @@
 package org.aion.vm.exception;
 
+/**
+ * @implNote The VMException is an Exception throws from the BulkExecutor {@link
+ *     org.aion.vm.BulkExecutor} when the vm has fatal situation occurs,e.g. OOM. The kernel will
+ *     handle this exception by shutting down the kernel immediately.
+ * @author Jay Tseng
+ */
 public class VMException extends Exception {
 
+    /** @param result the transaction executing result returned by the VM. */
     public VMException(String result) {
         super(result);
     }
