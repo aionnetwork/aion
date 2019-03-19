e3d61e5b094cc83d137c274fefab2347ea671fc2
AionJayT <jay.tseng@aion.network>
2019-03-19 15:57:32 -0400

    1.) refine AionBlockchainImpl classes import 2.) Fix engergy charge issue of the AVM transaction

--- modAionImpl/src/org/aion/zero/impl/AionBlockchainImpl.java
+++ modAionImpl/src/org/aion/zero/impl/AionBlockchainImpl.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Properties;
 import java.util.Set;
 import java.util.Stack;
 import java.util.concurrent.atomic.AtomicLong;
