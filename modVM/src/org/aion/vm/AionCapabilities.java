e15f484aa7423fd747168963b9e73ffe521acabb
geoffrey <geoffrey@aion.network>
2019-06-24 14:45:23 -0400

    Updated to work with AVM commit 845f5364b6db840cf5599515babd648d6265e93c

--- modVM/src/org/aion/vm/AionCapabilities.java
+++ modVM/src/org/aion/vm/AionCapabilities.java
@@ -1,9 +1,9 @@
 package org.aion.vm;
 
+import org.aion.avm.core.AvmTransaction;
 import org.aion.avm.core.IExternalCapabilities;
 import org.aion.avm.core.types.InternalTransaction;
 import org.aion.crypto.HashUtil;
-import org.aion.kernel.TestingTransaction;
 import org.aion.types.AionAddress;
 import org.aion.vm.api.interfaces.TransactionInterface;
 
@@ -35,8 +35,8 @@ public class AionCapabilities implements IExternalCapabilities {
     }
 
     @Override
-    public AionAddress generateContractAddress(TestingTransaction tx) {
-        return new AionAddress(HashUtil.calcNewAddr(tx.getSenderAddress().toByteArray(), tx.getNonce()));
+    public AionAddress generateContractAddress(AvmTransaction tx) {
+        return new AionAddress(HashUtil.calcNewAddr(tx.senderAddress.toByteArray(), tx.nonce.toByteArray()));
     }
 
     @Override
