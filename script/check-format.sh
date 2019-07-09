8b7985076c3fad93512fe2808b4f053ede4fe95b
Alexandra Roatis <alexandra@nuco.io>
2019-07-09 10:25:22 -0400

    Bug fix: The diff based formatting check does not work on Jenkins. Reverting to full codebase check.

--- script/check-format.sh
+++ script/check-format.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-branch="$(git rev-parse --abbrev-ref HEAD)"
-master_branch=master-pre-merge
-if [ "$branch" = "$master_branch" ]
-then
-    master_branch=master
-fi
+# this is not working on Jenkins but may be a useful idea later to reduce the check duration
+#branch="$(git rev-parse --abbrev-ref HEAD)"
+#master_branch=master-pre-merge
+#if [ "$branch" = "$master_branch" ]
+#then
+#    master_branch=master
+#fi
+#
+#echo "Checking out '$master_branch' branch for comparison:"
+#git checkout $master_branch; git checkout $branch
+#
+#diff_count="$(git log --graph --oneline --date=relative $master_branch..$branch  | wc -l)"
+#if [ $diff_count -eq 1 ]
+#then
+#    echo "There is $diff_count commit in addition to reference branch."
+#else
+#    echo "There are $diff_count commits in addition to reference branch."
+#fi
+#
+#java_files="$(git show HEAD~$diff_count..HEAD --name-only | grep 'mod.*java$' | sort -u | grep -v -E '3rdParty|modApiServer.src.org.aion.api.server.pb.Message.java' )"
 
-diff_count="$(git log --graph --oneline --date=relative $master_branch..$branch  | wc -l)"
-java_files="$(git show HEAD~$diff_count..HEAD --name-only | grep 'mod.*java$' | sort -u | grep -v -E '3rdParty|modApiServer.src.org.aion.api.server.pb.Message.java' )"
+java_files="$(find $(pwd) -maxdepth 100 -type f -not -path '*/\.*' | grep 'mod.*java$' | grep -v -E 'aion_fastvm|aion_gui|aion_vm_api|3rdParty|modApiServer.src.org.aion.api.server.pb.Message.java' )"
 jar="$(find $(pwd) -maxdepth 2 -type f -not -path '*/\.*' | grep 'google-java-format-1.7-all-deps.jar$')"
 for file in $java_files
 do
