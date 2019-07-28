44da22bbbcab8e186357fc0f4ff479cec79d7951
nuco-shidokht <shidokht@nuco.io>
2019-07-28 13:57:18 -0400

    set the genesisStakingBlock only once in config

--- modAionImpl/src/org/aion/zero/impl/config/CfgAion.java
+++ modAionImpl/src/org/aion/zero/impl/config/CfgAion.java
@@ -109,7 +109,9 @@ public final class CfgAion extends Cfg {
             setGenesis();
         }
 
-        genesisStakingBlock = new GenesisStakingBlock(genesis.getExtraData());
+        if(genesisStakingBlock == null) {
+            genesisStakingBlock = new GenesisStakingBlock(genesis.getExtraData());
+        }
 
         return genesisStakingBlock;
     }
