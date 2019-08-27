94728d7632bb19e7c1d4bad6a2e7d2d863d29168
geoffrey <geoffrey@aion.network>
2019-08-27 14:12:59 -0400

    Moved some methods between PendingState interfaces

--- modMcf/src/org/aion/mcf/blockchain/IPendingState.java
+++ modMcf/src/org/aion/mcf/blockchain/IPendingState.java
@@ -1,24 +1,8 @@
 package org.aion.mcf.blockchain;
 
-import java.math.BigInteger;
 import java.util.List;
 import org.aion.base.AionTransaction;
-import org.aion.mcf.db.RepositoryCache;
-import org.aion.types.AionAddress;
 
 public interface IPendingState {
-
-    List<TxResponse> addPendingTransactions(List<AionTransaction> transactions);
-
-    TxResponse addPendingTransaction(AionTransaction tx);
-
-    boolean isValid(AionTransaction tx);
-
-    RepositoryCache<?, ?> getRepository();
-
     List<AionTransaction> getPendingTransactions();
-
-    BigInteger bestPendingStateNonce(AionAddress addr);
-
-    String getVersion();
 }
