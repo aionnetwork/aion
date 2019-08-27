8bdbf0de442123cff1c1e3b6c4c966070c1a6026
geoffrey <geoffrey@aion.network>
2019-08-27 15:39:45 -0400

    Removed AbstractState

--- modMcf/src/org/aion/mcf/core/AccountState.java
+++ modMcf/src/org/aion/mcf/core/AccountState.java
@@ -10,7 +10,16 @@ import org.aion.rlp.RLPList;
 import org.aion.util.conversions.Hex;
 
 /** Account state. */
-public class AccountState extends AbstractState {
+public class AccountState {
+
+    /** The RLP encoding of this account state. */
+    protected byte[] rlpEncoded = null;
+
+    /** Flag indicating whether the account has been deleted. */
+    protected boolean deleted = false;
+
+    /** Flag indicating whether the state of the account has been changed. */
+    protected boolean dirty = false;
 
     /**
      * A value equal to the number of transactions sent from this address, or, in the case of
@@ -93,6 +102,53 @@ public class AccountState extends AbstractState {
 
         stateRoot = items.get(2).getRLPData();
         codeHash = items.get(3).getRLPData();
+    }
+
+    /**
+     * Checks whether the state of the account has been changed during execution.
+     *
+     * <p>The dirty status is set internally by the object when its stored values have been
+     * modified.
+     *
+     * @return {@code true} if the account state has been modified (including if the account is
+     *     newly created), {@code false} if the account state is the same as in the database
+     */
+    public boolean isDirty() {
+        return this.dirty;
+    }
+
+    /**
+     * Sets the dirty flag to true signaling that the object state has been changed.
+     *
+     * @apiNote Once the account has been modified (by setting the flag to {@code true}) it is
+     *     <b>considered dirty</b> even if its state reverts to the initial values by applying
+     *     subsequent changes.
+     * @implNote Method called internally by the account object when its state has been modified.
+     *     Resets the stored RLP encoding.
+     */
+    protected void makeDirty() {
+        this.rlpEncoded = null;
+        this.dirty = true;
+    }
+
+    /**
+     * Deletes the current account.
+     *
+     * @apiNote Once the account has been deleted (by setting the flag to {@code true}) <b>it cannot
+     *     be recovered</b>.
+     */
+    public void delete() {
+        this.deleted = true;
+        makeDirty();
+    }
+
+    /**
+     * Checks if the state of the account has been deleted.
+     *
+     * @return {@code true} if the account was deleted, {@code false} otherwise
+     */
+    public boolean isDeleted() {
+        return this.deleted;
     }
 
     /**
@@ -286,6 +342,13 @@ public class AccountState extends AbstractState {
         return accountStateCopy;
     }
 
+    /**
+     * Retrieves the RLP encoding of this object.
+     *
+     * @return a {@code byte} array representing the RLP encoding of the account state.
+     * @implNote For performance reasons, this encoding is stored when available and recomputed only
+     *     if the object has been modified during execution.
+     */
     public byte[] getEncoded() {
         if (rlpEncoded == null) {
             byte[] nonce = RLP.encodeBigInteger(this.nonce);
@@ -297,6 +360,14 @@ public class AccountState extends AbstractState {
         return rlpEncoded;
     }
 
+    /**
+     * Checks if the account state stores any meaningful information.
+     *
+     * @return {@code true} if the account state is empty, {@code false} otherwise
+     * @apiNote Empty accounts should not be stored in the database.
+     * @implNote Empty accounts do not have associated code (or by extension storage), i.e. cannot
+     *     be contract accounts.
+     */
     public boolean isEmpty() {
         return Arrays.equals(codeHash, EMPTY_DATA_HASH)
                 && BigInteger.ZERO.equals(balance)
