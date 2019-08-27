336e6ab5734d5f243c2368cc28cf3875eb57b90b
geoffrey <geoffrey@aion.network>
2019-08-27 14:12:59 -0400

    Moved some methods between PendingState interfaces

--- modMcf/src/org/aion/mcf/blockchain/IPendingStateInternal.java
+++ modMcf/src/org/aion/mcf/blockchain/IPendingStateInternal.java
@@ -1,5 +1,11 @@
 package org.aion.mcf.blockchain;
 
+import java.math.BigInteger;
+import java.util.List;
+import org.aion.base.AionTransaction;
+import org.aion.mcf.db.RepositoryCache;
+import org.aion.types.AionAddress;
+
 /**
  * Internal pending state interface.
  */
@@ -16,4 +22,16 @@ public interface IPendingStateInternal extends IPendingState {
     void loadPendingTx();
 
     void checkAvmFlag();
+
+    BigInteger bestPendingStateNonce(AionAddress addr);
+
+    String getVersion();
+
+    List<TxResponse> addPendingTransactions(List<AionTransaction> transactions);
+
+    TxResponse addPendingTransaction(AionTransaction tx);
+
+    boolean isValid(AionTransaction tx);
+
+    RepositoryCache<?, ?> getRepository();
 }
