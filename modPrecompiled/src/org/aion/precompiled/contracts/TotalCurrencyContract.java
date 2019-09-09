aionnetwork/aion

commit fc52d22731387e0599a77356fac551469d8325b4

Now use TransactionStatus in Precompiled

modPrecompiled/src/org/aion/precompiled/contracts/TotalCurrencyContract.java
@@ -3,13 +3,13 @@ package org.aion.precompiled.contracts;
 import java.math.BigInteger;
 import org.aion.crypto.ed25519.ECKeyEd25519;
 import org.aion.crypto.ed25519.Ed25519Signature;
-import org.aion.precompiled.PrecompiledResultCode;
 import org.aion.precompiled.PrecompiledTransactionResult;
 import org.aion.precompiled.type.IPrecompiledDataWord;
 import org.aion.precompiled.type.IExternalStateForPrecompiled;
 import org.aion.precompiled.type.PrecompiledContract;
 import org.aion.precompiled.type.PrecompiledDataWord;
 import org.aion.types.AionAddress;
+import org.aion.types.TransactionStatus;
 import org.aion.util.biginteger.BIUtil;
 
 /** A pre-compiled contract for retrieving and updating the total amount of currency. */
@@ -91,23 +91,23 @@ public class TotalCurrencyContract implements PrecompiledContract {
     private PrecompiledTransactionResult queryNetworkBalance(int input, long nrg) {
         if (nrg < COST) {
             // TODO: should this cost be the same as updating state (probably not?)
-            return new PrecompiledTransactionResult(PrecompiledResultCode.OUT_OF_NRG, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("OUT_OF_NRG"), 0);
         }
 
         IPrecompiledDataWord balanceData =
                 this.externalState.getStorageValue(this.address, PrecompiledDataWord.fromInt(input));
         return new PrecompiledTransactionResult(
-                PrecompiledResultCode.SUCCESS, nrg - COST, balanceData.copyOfData());
+            TransactionStatus.successful(), nrg - COST, balanceData.copyOfData());
     }
 
     private PrecompiledTransactionResult executeUpdateTotalBalance(byte[] input, long nrg) {
         // update total portion
         if (nrg < COST) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.OUT_OF_NRG, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("OUT_OF_NRG"), 0);
         }
 
         if (input.length < 114) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
         }
 
         // process input data
@@ -128,7 +128,7 @@ public class TotalCurrencyContract implements PrecompiledContract {
         // verify signature is correct
         Ed25519Signature sig = Ed25519Signature.fromBytes(sign);
         if (sig == null) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
         }
 
         byte[] payload = new byte[18];
@@ -136,12 +136,12 @@ public class TotalCurrencyContract implements PrecompiledContract {
         boolean b = ECKeyEd25519.verify(payload, sig.getSignature(), sig.getPubkey(null));
 
         if (!b) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
         }
 
         // verify public key matches owner
         if (!this.ownerAddress.equals(new AionAddress(sig.getAddress()))) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
         }
 
         // payload processing
@@ -151,7 +151,7 @@ public class TotalCurrencyContract implements PrecompiledContract {
         BigInteger value = BIUtil.toBI(amount);
 
         if (signum != 0x0 && signum != 0x1) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
         }
 
         BigInteger finalValue;
@@ -161,7 +161,7 @@ public class TotalCurrencyContract implements PrecompiledContract {
         } else {
             // subtraction
             if (value.compareTo(totalCurrBI) > 0) {
-                return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+                return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
             }
 
             finalValue = totalCurrBI.subtract(value);
@@ -172,6 +172,6 @@ public class TotalCurrencyContract implements PrecompiledContract {
                 this.address,
                 chainId,
                 PrecompiledDataWord.fromBytes(finalValue.toByteArray()));
-        return new PrecompiledTransactionResult(PrecompiledResultCode.SUCCESS, nrg - COST);
+        return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), nrg - COST);
     }
 }

modPrecompiled/src/org/aion/precompiled/PrecompiledResultCode.java
@@ -1,96 +1,0 @@
-package org.aion.precompiled;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/** An enumeration representing the execution status of a transaction. */
-public enum PrecompiledResultCode {
-    SUCCESS(0, ResultCategory.SUCCESS),
-
-    INVALID_NONCE(101, ResultCategory.REJECTED),
-
-    INVALID_NRG_LIMIT(102, ResultCategory.REJECTED),
-
-    INSUFFICIENT_BALANCE(103, ResultCategory.REJECTED),
-
-    FAILURE(1, ResultCategory.FAILED),
-
-    OUT_OF_NRG(2, ResultCategory.FAILED),
-
-    BAD_INSTRUCTION(3, ResultCategory.FAILED),
-
-    BAD_JUMP_DESTINATION(4, ResultCategory.FAILED),
-
-    STACK_OVERFLOW(5, ResultCategory.FAILED),
-
-    STACK_UNDERFLOW(6, ResultCategory.FAILED),
-
-    REVERT(7, ResultCategory.REVERT),
-
-    STATIC_MODE_ERROR(8, ResultCategory.FAILED),
-
-    INCOMPATIBLE_CONTRACT_CALL(9, ResultCategory.FAILED),
-
-    ABORT(11, ResultCategory.FAILED),
-
-    VM_REJECTED(-1, ResultCategory.FATAL),
-
-    VM_INTERNAL_ERROR(-2, ResultCategory.FATAL);
-
-    private enum ResultCategory {
-        SUCCESS,
-        REJECTED,
-        FAILED,
-        FATAL,
-        REVERT
-    }
-
-    private static Map<Integer, PrecompiledResultCode> integerMapping = new HashMap<>();
-    private ResultCategory category;
-    private int value;
-
-    static {
-        for (PrecompiledResultCode code : PrecompiledResultCode.values()) {
-            integerMapping.put(code.value, code);
-        }
-    }
-
-    PrecompiledResultCode(int value, ResultCategory category) {
-        this.value = value;
-        this.category = category;
-    }
-
-    public boolean isSuccess() {
-        return this.category == ResultCategory.SUCCESS;
-    }
-
-    public boolean isRejected() {
-        return this.category == ResultCategory.REJECTED;
-    }
-
-    public boolean isFailed() {
-        return ((this.category == ResultCategory.FAILED)
-                || (this.category == ResultCategory.REVERT));
-    }
-
-    public boolean isFatal() {
-        return this.category == ResultCategory.FATAL;
-    }
-
-    public boolean isRevert() {
-        return this.category == ResultCategory.REVERT;
-    }
-
-    public int toInt() {
-        return this.value;
-    }
-
-    public static PrecompiledResultCode fromInt(int code) {
-        PrecompiledResultCode result = integerMapping.get(code);
-        if (result == null) {
-            throw new IllegalArgumentException(
-                    "No FastVmResultCode whose integer representation is: " + code);
-        }
-        return result;
-    }
-}

modPrecompiled/src/org/aion/precompiled/PrecompiledSideEffects.java
@@ -1,123 +1,0 @@
-/*
- * Copyright (c) 2017-2018 Aion foundation.
- *
- *     This file is part of the aion network project.
- *
- *     The aion network project is free software: you can redistribute it
- *     and/or modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation, either version 3 of
- *     the License, or any later version.
- *
- *     The aion network project is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied
- *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *     See the GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with the aion network project source files.
- *     If not, see <https://www.gnu.org/licenses/>.
- *
- * Contributors:
- *     Aion foundation.
- */
-package org.aion.precompiled;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import org.aion.types.AionAddress;
-import org.aion.types.InternalTransaction;
-import org.aion.types.Log;
-
-public class PrecompiledSideEffects {
-
-    private Set<AionAddress> deleteAccounts = new HashSet<>();
-    private List<InternalTransaction> internalTxs = new ArrayList<>();
-    private List<Log> logs = new ArrayList<>();
-
-    public void addToDeletedAddresses(AionAddress address) {
-        deleteAccounts.add(address);
-    }
-
-    /**
-     * Adds the collection addresses to the set of deleted accounts if addresses is non-null.
-     *
-     * @param addresses The addressed to add to the set of deleted accounts.
-     */
-    public void addAllToDeletedAddresses(Collection<AionAddress> addresses) {
-        for (AionAddress addr : addresses) {
-            if (addr != null) {
-                addToDeletedAddresses(addr);
-            }
-        }
-    }
-
-    /**
-     * Adds log to the execution logs.
-     *
-     * @param log The log to add to the execution logs.
-     */
-    public void addLog(Log log) {
-        logs.add(log);
-    }
-
-    /**
-     * Adds a collection of logs, logs, to the execution logs.
-     *
-     * @param logs The collection of logs to add to the execution logs.
-     */
-    public void addLogs(Collection<Log> logs) {
-        for (Log log : logs) {
-            if (log != null) {
-                addLog(log);
-            }
-        }
-    }
-
-    /**
-     * Adds an internal transaction, tx, to the internal transactions list.
-     *
-     * @param tx The internal transaction to add.
-     */
-    public void addInternalTransaction(InternalTransaction tx) {
-        internalTxs.add(tx);
-    }
-
-    /**
-     * Adds a collection of internal transactions, txs, to the internal transactions list.
-     *
-     * @param txs The collection of internal transactions to add.
-     */
-    public void addInternalTransactions(List<InternalTransaction> txs) {
-        for (InternalTransaction tx : txs) {
-            if (tx != null) {
-                addInternalTransaction(tx);
-            }
-        }
-    }
-
-    public void merge(PrecompiledSideEffects other) {
-        addInternalTransactions(other.getInternalTransactions());
-        addAllToDeletedAddresses(other.getAddressesToBeDeleted());
-        addLogs(other.getExecutionLogs());
-    }
-
-    public List<AionAddress> getAddressesToBeDeleted() {
-        return new ArrayList<>(deleteAccounts);
-    }
-
-    public List<Log> getExecutionLogs() {
-        return logs;
-    }
-
-    /**
-     * Returns the internal transactions.
-     *
-     * @return the internal transactions.
-     */
-    public List<InternalTransaction> getInternalTransactions() {
-        return internalTxs;
-    }
-}

modPrecompiled/src/org/aion/precompiled/PrecompiledTransactionResult.java
@@ -1,27 +1,21 @@
 package org.aion.precompiled;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.ArrayList;
-import java.util.List;
-import org.aion.types.AionAddress;
-import org.aion.types.InternalTransaction;
-import org.aion.types.Log;
+import org.aion.types.TransactionStatus;
 import org.aion.util.bytes.ByteUtil;
 
 public class PrecompiledTransactionResult {
 
-    private PrecompiledResultCode code;
+    private TransactionStatus code;
     private byte[] output;
     private long energyRemaining;
 
     /**
      * Constructs a new {@code TransactionResult} with no side-effects, with zero energy remaining,
-     * with an empty byte array as its output and {@link PrecompiledResultCode#SUCCESS} as its
+     * with an empty byte array as its output and {@link TransactionStatus#successful()} as its
      * result code.
      */
     public PrecompiledTransactionResult() {
-        this.code = PrecompiledResultCode.SUCCESS;
+        this.code = TransactionStatus.successful();
         this.output = new byte[0];
         this.energyRemaining = 0;
     }
@@ -30,11 +24,11 @@ public class PrecompiledTransactionResult {
      * Constructs a new {@code TransactionResult} with no side-effects and with the specified result
      * code and remaining energy.
      *
-     * @param code The transaction result code.
+     * @param status The transaction result code.
      * @param energyRemaining The energy remaining after executing the transaction.
      */
-    public PrecompiledTransactionResult(PrecompiledResultCode code, long energyRemaining) {
-        this.code = code;
+    public PrecompiledTransactionResult(TransactionStatus status, long energyRemaining) {
+        this.code = status;
         this.energyRemaining = energyRemaining;
         this.output = new byte[0];
     }
@@ -43,76 +37,31 @@ public class PrecompiledTransactionResult {
      * Constructs a new {@code TransactionResult} with no side-effects and with the specified result
      * code, remaining energy and output.
      *
-     * @param code The transaction result code.
+     * @param status The transaction result code.
      * @param energyRemaining The energy remaining after executing the transaction.
      * @param output The output of executing the transaction.
      */
     public PrecompiledTransactionResult(
-            PrecompiledResultCode code, long energyRemaining, byte[] output) {
-        this.code = code;
+            TransactionStatus status, long energyRemaining, byte[] output) {
+        this.code = status;
         this.output = (output == null) ? new byte[0] : output;
         this.energyRemaining = energyRemaining;
     }
 
-    /**
-     * Returns a <i>partial</i> byte array representation of this {@code TransactionResult}.
-     *
-     * <p>The representation is partial because it only represents the {@link
-     * PrecompiledResultCode}, the amount of energy remaining, and the output.
-     *
-     * @return A partial byte array representation of this object.
-     */
-    public byte[] toBytes() {
-        ByteBuffer buffer =
-                ByteBuffer.allocate(
-                        Integer.BYTES + Long.BYTES + Integer.BYTES + this.output.length);
-        buffer.order(ByteOrder.BIG_ENDIAN);
-        buffer.putInt(this.code.toInt());
-        buffer.putLong(this.energyRemaining);
-        buffer.putInt(this.output.length);
-        buffer.put(this.output);
-        return buffer.array();
-    }
-
     // TODO: document exception / maybe catch it and throw something more informative
 
-    /**
-     * Returns a {@code TransactionResult} object from a partial byte array representation obtained
-     * via the {@code toBytes()} method.
-     *
-     * @param bytes A partial byte array representation of a {@code TransactionResult}.
-     * @return The {@code TransactionResult} object obtained from the byte array representation.
-     */
-    public static PrecompiledTransactionResult fromBytes(byte[] bytes) {
-        ByteBuffer buffer = ByteBuffer.wrap(bytes);
-        buffer.order(ByteOrder.BIG_ENDIAN);
-
-        PrecompiledResultCode code = PrecompiledResultCode.fromInt(buffer.getInt());
-        long energyRemaining = buffer.getLong();
-
-        int outputLength = buffer.getInt();
-        byte[] output = new byte[outputLength];
-        buffer.get(output);
-
-        return new PrecompiledTransactionResult(code, energyRemaining, output);
-    }
-
-    public void setResultCode(PrecompiledResultCode code) {
+    public void setResultCode(TransactionStatus code) {
         if (code == null) {
             throw new NullPointerException("Cannot set null result code.");
         }
         this.code = code;
     }
 
-    public void setReturnData(byte[] output) {
-        this.output = (output == null) ? new byte[0] : output;
-    }
-
     public void setEnergyRemaining(long energyRemaining) {
         this.energyRemaining = energyRemaining;
     }
 
-    public PrecompiledResultCode getResultCode() {
+    public TransactionStatus getStatus() {
         return this.code;
     }
 

modPrecompiled/src/org/aion/precompiled/contracts/ATB/BridgeController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import javax.annotation.Nonnull;
 import org.aion.crypto.ISignature;
 import org.aion.crypto.SignatureFac;
-import org.aion.precompiled.PrecompiledResultCode;
 import org.aion.precompiled.PrecompiledTransactionResult;
 import org.aion.precompiled.PrecompiledUtilities;
 import org.aion.types.AionAddress;
@@ -275,13 +274,12 @@ public class BridgeController {
              */
             PrecompiledTransactionResult result;
             if ((result = transferable.transfer(b.getRecipient(), b.getTransferValue()))
-                            .getResultCode()
-                    == PrecompiledResultCode.FAILURE)
+                            .getStatus().isFailed())
                 // no need to return list of transactions, since they're all being dropped
                 return processError(ErrCode.INVALID_TRANSFER);
 
             // otherwise if transfer was successful
-            if (result.getResultCode() == PrecompiledResultCode.SUCCESS)
+            if (result.getStatus().isSuccess())
                 if (!emitDistributed(
                         b.getSourceTransactionHash(), b.getRecipient(), b.getTransferValue()))
                     return processError(ErrCode.INVALID_TRANSFER);

modPrecompiled/src/org/aion/precompiled/contracts/ATB/TokenBridgeContract.java
@@ -11,7 +11,6 @@ import static org.aion.precompiled.contracts.ATB.BridgeUtilities.orDefaultDword;
 
 import java.math.BigInteger;
 import javax.annotation.Nonnull;
-import org.aion.precompiled.PrecompiledResultCode;
 import org.aion.precompiled.PrecompiledTransactionResult;
 import org.aion.precompiled.type.IExternalStateForPrecompiled;
 import org.aion.precompiled.type.PrecompiledContract;
@@ -19,6 +18,7 @@ import org.aion.precompiled.type.PrecompiledTransactionContext;
 import org.aion.types.AionAddress;
 import org.aion.types.InternalTransaction;
 import org.aion.types.InternalTransaction.RejectedStatus;
+import org.aion.types.TransactionStatus;
 
 public class TokenBridgeContract implements PrecompiledContract, Transferable {
 
@@ -215,7 +215,7 @@ public class TokenBridgeContract implements PrecompiledContract, Transferable {
     }
 
     private static final PrecompiledTransactionResult THROW =
-            new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
 
     private PrecompiledTransactionResult fail() {
         this.context.markAllInternalTransactionsAsRejected();
@@ -224,7 +224,7 @@ public class TokenBridgeContract implements PrecompiledContract, Transferable {
 
     private PrecompiledTransactionResult success() {
         long energyRemaining = this.context.transactionEnergy - ENERGY_CONSUME;
-        return new PrecompiledTransactionResult(PrecompiledResultCode.SUCCESS, energyRemaining);
+        return new PrecompiledTransactionResult(TransactionStatus.successful(), energyRemaining);
     }
 
     private PrecompiledTransactionResult success(@Nonnull final byte[] response) {
@@ -232,7 +232,7 @@ public class TokenBridgeContract implements PrecompiledContract, Transferable {
         long energyRemaining = this.context.transactionEnergy - ENERGY_CONSUME;
         assert energyRemaining >= 0;
         return new PrecompiledTransactionResult(
-                PrecompiledResultCode.SUCCESS, energyRemaining, response);
+                TransactionStatus.successful(), energyRemaining, response);
     }
 
     private boolean isFromAddress(byte[] address) {
@@ -256,7 +256,7 @@ public class TokenBridgeContract implements PrecompiledContract, Transferable {
             @Nonnull final byte[] dest, @Nonnull final BigInteger value) {
         // some initial checks, treat as failure
         if (this.externalState.getBalance(this.contractAddress).compareTo(value) < 0)
-            return new PrecompiledTransactionResult(PrecompiledResultCode.FAILURE, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), 0);
 
         // assemble an internal transaction
         AionAddress sender = this.contractAddress;
@@ -283,6 +283,6 @@ public class TokenBridgeContract implements PrecompiledContract, Transferable {
         this.externalState.addBalance(destination, value);
 
         // construct result
-        return new PrecompiledTransactionResult(PrecompiledResultCode.SUCCESS, 0);
+        return new PrecompiledTransactionResult(TransactionStatus.successful(), 0);
     }
 }

modPrecompiled/src/org/aion/precompiled/contracts/Blake2bHashContract.java
@@ -3,9 +3,9 @@ package org.aion.precompiled.contracts;
 import static org.aion.crypto.HashUtil.blake256;
 
 import com.google.common.annotations.VisibleForTesting;
-import org.aion.precompiled.PrecompiledResultCode;
 import org.aion.precompiled.PrecompiledTransactionResult;
 import org.aion.precompiled.type.PrecompiledContract;
+import org.aion.types.TransactionStatus;
 
 /**
  * @author Jay Tseng
@@ -34,7 +34,7 @@ public class Blake2bHashContract implements PrecompiledContract {
         // check length
         if (input == null || input.length == 0 || input.length > 2_097_152L) {
             return new PrecompiledTransactionResult(
-                    PrecompiledResultCode.FAILURE,
+                    TransactionStatus.nonRevertedFailure("FAILURE"),
                     nrg - COST,
                     INPUT_LENGTH_ERROR_MESSAGE.getBytes());
         }
@@ -46,7 +46,7 @@ public class Blake2bHashContract implements PrecompiledContract {
         long nrgLeft = nrg - (COST + additionalNRG);
 
         if (nrgLeft < 0) {
-            return new PrecompiledTransactionResult(PrecompiledResultCode.OUT_OF_NRG, 0);
+            return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("OUT_OF_NRG"), 0);
         }
 
         return blake256Hash(input, nrgLeft);
@@ -54,7 +54,7 @@ public class Blake2bHashContract implements PrecompiledContract {
 
     private PrecompiledTransactionResult blake256Hash(byte[] input, long nrg) {
         byte[] hash = blake256(input);
-        return new PrecompiledTransactionResult(PrecompiledResultCode.SUCCESS, nrg, hash);
+        return new PrecompiledTransactionResult(TransactionStatus.successful(), nrg, hash);
     }
 
     @VisibleForTesting

commit 079d00b167a00ea553a7b72d00620e4a0406e1c4

Fixed bug introduced in commit fc52d22731387e0599a77356fac551469d8325b4

modPrecompiled/src/org/aion/precompiled/contracts/TotalCurrencyContract.java
@@ -172,6 +172,6 @@ public class TotalCurrencyContract implements PrecompiledContract {
                 this.address,
                 chainId,
                 PrecompiledDataWord.fromBytes(finalValue.toByteArray()));
-        return new PrecompiledTransactionResult(TransactionStatus.nonRevertedFailure("FAILURE"), nrg - COST);
+        return new PrecompiledTransactionResult(TransactionStatus.successful(), nrg - COST);
     }
 }