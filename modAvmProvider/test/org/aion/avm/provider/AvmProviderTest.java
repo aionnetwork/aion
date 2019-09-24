d0868c587fbcb546ff74baef586e83ca8d79f1bb
aionick <nick@nuco.io>
2019-09-24 15:17:46 -0400

    AKI-315: Added tests for the modAvmProvider module
    - These tests had to wait until the kernel was using the multi-versioned
      avm, which it now is. They test a few of the central classes that this
      module exposes.

---
+++ modAvmProvider/test/org/aion/avm/provider/AvmProviderTest.java
@@ -0,0 +1,140 @@
+package org.aion.avm.provider;
+
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+import org.aion.avm.provider.resources.PathManager;
+import org.aion.avm.stub.AvmExecutionType;
+import org.aion.avm.stub.AvmVersion;
+import org.aion.avm.stub.IAionVirtualMachine;
+import org.aion.avm.stub.IAvmExternalState;
+import org.aion.avm.stub.IAvmFutureResult;
+import org.aion.avm.stub.IEnergyRules;
+import org.aion.avm.stub.IEnergyRules.TransactionType;
+import org.aion.base.AccountState;
+import org.aion.mcf.db.IBlockStoreBase;
+import org.aion.mcf.db.RepositoryCache;
+import org.aion.types.AionAddress;
+import org.aion.types.Transaction;
+import org.aion.types.TransactionResult;
+import org.aion.zero.impl.blockchain.StandaloneBlockchain;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests more generic aspects of the {@link AvmProvider} class (specific version testing is done in
+ * the the test class {@link AvmProviderVersion1Test}.
+ *
+ * What's being tested here are methods related specifically to the provider at large, or more integ
+ * style tests.
+ */
+public class AvmProviderTest {
+    private String projectRootDir;
+
+    @Before
+    public void setup() throws Exception {
+        // Ensure we always begin with a disabled avm version.
+        if (!AvmProvider.holdsLock()) {
+            Assert.assertTrue(AvmProvider.tryAcquireLock(1, TimeUnit.MINUTES));
+        }
+        AvmProvider.disableAvmVersion(AvmVersion.VERSION_1);
+        AvmProvider.releaseLock();
+        this.projectRootDir = PathManager.fetchProjectRootDir();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        // Ensure we never exit with an enabled avm version.
+        if (!AvmProvider.holdsLock()) {
+            Assert.assertTrue(AvmProvider.tryAcquireLock(1, TimeUnit.MINUTES));
+        }
+        AvmProvider.disableAvmVersion(AvmVersion.VERSION_1);
+        AvmProvider.releaseLock();
+    }
+
+    @Test(expected = IllegalMonitorStateException.class)
+    public void testReleaseLockWhenCallerDoesNotOwnIt() {
+        AvmProvider.releaseLock();
+    }
+
+    @Test
+    public void testBalanceTransferTransactionVersion1() throws Exception {
+        Assert.assertTrue(AvmProvider.tryAcquireLock(1, TimeUnit.MINUTES));
+        AvmProvider.enableAvmVersion(AvmVersion.VERSION_1, this.projectRootDir);
+        AvmProvider.startAvm(AvmVersion.VERSION_1);
+
+        // Set up the repo and give the sender account some balance.
+        RepositoryCache<AccountState, IBlockStoreBase> repository = newRepository();
+        AionAddress sender = randomAddress();
+        AionAddress recipient = randomAddress();
+        addBalance(repository, sender, BigInteger.valueOf(1_000_000));
+
+        // Run the transaction.
+        RepositoryCache<AccountState, IBlockStoreBase> repositoryChild = repository.startTracking();
+        IAvmExternalState externalState = newExternalState(AvmVersion.VERSION_1, repositoryChild, newEnergyRules());
+        Transaction transaction = newBalanceTransferTransaction(sender, recipient, BigInteger.TEN);
+        IAionVirtualMachine avm = AvmProvider.getAvm(AvmVersion.VERSION_1);
+        IAvmFutureResult[] futures = avm.run(externalState, new Transaction[]{ transaction }, AvmExecutionType.MINING, 0);
+
+        // Assert the result and state changes we expect.
+        Assert.assertEquals(1, futures.length);
+        TransactionResult result = futures[0].getResult();
+        Assert.assertTrue(result.transactionStatus.isSuccess());
+        Assert.assertEquals(BigInteger.TEN, repositoryChild.getBalance(recipient));
+
+        AvmProvider.shutdownAvm(AvmVersion.VERSION_1);
+        AvmProvider.disableAvmVersion(AvmVersion.VERSION_1);
+        AvmProvider.releaseLock();
+    }
+
+    private void addBalance(RepositoryCache<AccountState, IBlockStoreBase> repository, AionAddress account, BigInteger amount) {
+        RepositoryCache cache = repository.startTracking();
+        cache.addBalance(account, amount);
+        cache.flush();
+    }
+
+    private Transaction newBalanceTransferTransaction(AionAddress sender, AionAddress recipient, BigInteger value) {
+        return Transaction.contractCallTransaction(sender, recipient, new byte[32], BigInteger.ZERO, value, new byte[0], 50_000, 1);
+    }
+
+    private RepositoryCache<AccountState, IBlockStoreBase> newRepository() {
+        return new StandaloneBlockchain.Builder()
+            .withDefaultAccounts()
+            .withValidatorConfiguration("simple")
+            .withAvmEnabled()
+            .build()
+            .bc
+            .getRepository()
+            .startTracking();
+    }
+
+    private IEnergyRules newEnergyRules() {
+        return (t, l) -> {
+            if (t == TransactionType.CREATE) {
+                return (200_000 <= l) && (l <= 5_000_000);
+            } else {
+                return (21_000 <= l) && (l <= 2_000_000);
+            }
+        };
+    }
+
+    private IAvmExternalState newExternalState(AvmVersion version, RepositoryCache<AccountState, IBlockStoreBase> repository, IEnergyRules energyRules) {
+        return AvmProvider.newExternalStateBuilder(version)
+            .withRepository(repository)
+            .withEnergyRules(energyRules)
+            .withMiner(randomAddress())
+            .withDifficulty(BigInteger.ONE)
+            .withBlockEnergyLimit(15_000_000)
+            .withBlockNumber(1)
+            .withBlockTimestamp(0)
+            .allowNonceIncrement(true)
+            .isLocalCall(false)
+            .build();
+    }
+
+    private static AionAddress randomAddress() {
+        return new AionAddress(RandomUtils.nextBytes(AionAddress.LENGTH));
+    }
+}
---
+++ modAvmProvider/test/org/aion/avm/provider/resources/PathManager.java
@@ -0,0 +1,17 @@
+package org.aion.avm.provider.resources;
+
+import org.junit.Assert;
+
+public final class PathManager {
+
+    /**
+     * Returns the project root directory. This assumes that the tests are being run from a directory
+     * named modAvmProvider inside the aion directory!
+     */
+    public static String fetchProjectRootDir() {
+        String workingDir = System.getProperty("user.dir");
+        int indexOfRoot = workingDir.lastIndexOf("modAvmProvider");
+        Assert.assertTrue(indexOfRoot >= 0);
+        return workingDir.substring(0, indexOfRoot);
+    }
+}
