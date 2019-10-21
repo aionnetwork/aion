44477ea2fe4beedec52b43e11879a04862d9c767
geoffrey <geoffrey@aion.network>
2019-10-21 12:05:43 -0400

    Removed RepositoryQuery

--- modMcf/src/org/aion/mcf/db/Repository.java
+++ modMcf/src/org/aion/mcf/db/Repository.java
@@ -1,15 +1,19 @@
 package org.aion.mcf.db;
 
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.aion.types.AionAddress;
+import org.aion.util.types.ByteArrayWrapper;
 
 /**
  * Database-like functionality.
  *
  * @apiNote Allows only batch operations on data.
  */
-public interface Repository<AS> extends RepositoryQuery<AS> {
+public interface Repository<AS> {
 
     /**
      * Creates a tracker repository for caching future changes.
@@ -107,10 +111,166 @@ public interface Repository<AS> extends RepositoryQuery<AS> {
     InternalVmType getVMUsed(AionAddress contract, byte[] codeHash);
 
     /**
+     * Returns the transaction type used to deploy the contract indicating which VM was used.
+     *
+     * @return the transaction type used to deploy the contract indicating which VM was used
+     */
+    InternalVmType getVmType(AionAddress contract);
+
+    /**
      * Set the transformed code to the account associated with the given address.
      *
      * @param address the address of the account of interest
      * @param code the transformed code
      */
     void setTransformedCode(AionAddress address, byte[] code);
+
+    /**
+     * Retrieves the transformed code for the account associated with the given address.
+     *
+     * @param address the address of the account of interest
+     * @return the transformed code associated to the account in {@code byte} array format
+     */
+    byte[] getTransformedCode(AionAddress address);
+
+    /**
+     * Retrieves the code for the account associated with the given address.
+     *
+     * @param address the address of the account of interest
+     * @return the code associated to the account in {@code byte} array format
+     */
+    byte[] getCode(AionAddress address);
+
+    /**
+     * Checks if the database contains an account state associated with the given address.
+     *
+     * @param address the address of the account of interest
+     * @return {@code true} if the account exists, {@code false} otherwise
+     */
+    boolean hasAccountState(AionAddress address);
+
+    /**
+     * Loads the account (and contract) state associated with the given address into the given hash
+     * maps.
+     *
+     * @param address the address of the account of interest
+     * @param accounts a map representing a cache of {@link AS} where the account state will be
+     *     loaded
+     * @param details a map representing a cache of {@link ContractDetails >} where the contract
+     *     details will be loaded
+     */
+    void loadAccountState(
+        AionAddress address,
+        Map<AionAddress, AS> accounts,
+        Map<AionAddress, ContractDetails> details);
+
+    /**
+     * Retrieves the current state of the account associated with the given address.
+     *
+     * @param address the address of the account of interest
+     * @return a {@link AS} object representing the account state as is stored in the database or
+     *     cache
+     */
+    AS getAccountState(AionAddress address);
+
+    /**
+     * Retrieves the current balance of the account associated with the given address.
+     *
+     * @param address the address of the account of interest
+     * @return a {@link BigInteger} value representing the current account balance
+     */
+    BigInteger getBalance(AionAddress address);
+
+    /**
+     * Retrieves the current nonce of the account associated with the given address.
+     *
+     * @param address the address of the account of interest
+     * @return a {@link BigInteger} value representing the current account nonce
+     */
+    BigInteger getNonce(AionAddress address);
+
+    /**
+     * Checks if the database contains contract details associated with the given address.
+     *
+     * @param addr the address of the account of interest
+     * @return {@code true} if there are contract details associated with the account, {@code false}
+     *     otherwise
+     */
+    boolean hasContractDetails(AionAddress addr);
+
+    /**
+     * Retrieves the contract details of the account associated with the given address.
+     *
+     * @param addr the address of the account of interest
+     * @return a {@link ContractDetails} object representing the contract details as are stored in
+     *     the database or cache
+     */
+    ContractDetails getContractDetails(AionAddress addr);
+
+    /**
+     * Returns a byte array from contract storage representing an encoding of the object graph for
+     * the given contract.
+     *
+     * @param contract the account address
+     * @return a byte array from contract storage representing an encoding of the object graph for
+     *     the given contract
+     */
+    byte[] getObjectGraph(AionAddress contract);
+
+    /**
+     * Retrieves the entries for the specified key values stored at the account associated with the
+     * given address.
+     *
+     * @param address the address of the account of interest
+     * @param keys the collection of keys of interest (which may be {@code null})
+     * @return the storage entries for the specified keys, or the full storage if the key collection
+     *     is {@code null}
+     * @apiNote When called with a null key collection, the method retrieves all the storage keys.
+     */
+    Map<ByteArrayWrapper, ByteArrayWrapper> getStorage(AionAddress address, Collection<ByteArrayWrapper> keys);
+
+    //    /**
+    //     * Retrieves the storage size the account associated with the given address.
+    //     *
+    //     * @param address
+    //     *            the address of the account of interest
+    //     * @return the number of storage entries for the given account
+    //     */
+    //    int getStorageSize(Address address);
+    //
+    //    /**
+    //     * Retrieves all the storage keys for the account associated with the given
+    //     * address.
+    //     *
+    //     * @param address
+    //     *            the address of the account of interest
+    //     * @return the set of storage keys, or an empty set if the given account
+    //     *         address does not exist
+    //     */
+    //    Set<ByteArrayByteArrayWrapper> getStorageKeys(Address address);
+
+    /**
+     * Retrieves the stored value for the specified key stored at the account associated with the
+     * given address.
+     *
+     * @param address the address of the account of interest
+     * @param key the key of interest
+     * @return a {@link ByteArrayWrapper} representing the data associated with the given key
+     */
+    ByteArrayWrapper getStorageValue(AionAddress address, ByteArrayWrapper key);
+
+    /**
+     * Retrieves the stored transactions for recovering pool tx.
+     *
+     * @return the list of transactions encoded bytes.
+     */
+    List<byte[]> getPoolTx();
+
+    /**
+     * Retrieves the stored transactions for recovering caching tx.
+     *
+     * @return the list of transactions encoded bytes.
+     */
+    List<byte[]> getCacheTx();
+
 }
