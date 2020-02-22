d7562946d42c84a3c1a301b85e41c7dbeb6d7d6b
AionJayT <jay.tseng@aion.network>
2020-02-21 19:40:26 -0500

    added txpool constant for the txpool initial arguments settings

--- modTxPool/src/org/aion/txpool/Constant.java
+++ modTxPool/src/org/aion/txpool/Constant.java
@@ -2,5 +2,9 @@ package org.aion.txpool;
 
 public class Constant {
 
-    public static final int MAX_BLK_SIZE = 2 * 1024 * 1024;
+    public final static int MAX_BLK_SIZE = 2 * 1024 * 1024;
+    public final static long MIN_ENERGY_CONSUME = 21_000L;
+    public final static String PROP_TX_TIMEOUT = "tx-timeout";
+    public final static String PROP_BLOCK_NRG_LIMIT = "blk-nrg-limit";
+    public final static String PROP_POOL_SIZE_MAX = "pool-size-max";
 }
