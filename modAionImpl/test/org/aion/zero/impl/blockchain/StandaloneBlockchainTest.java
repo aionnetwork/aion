1a8603caf884535d3295f4779a7e23d281adac64
AionJayT <jay.tseng@aion.network>
2020-03-19 10:48:44 -0400

    Add test for kernel internal shutdown

--- modAionImpl/test/org/aion/zero/impl/blockchain/StandaloneBlockchainTest.java
+++ modAionImpl/test/org/aion/zero/impl/blockchain/StandaloneBlockchainTest.java
@@ -4,12 +4,19 @@ import static com.google.common.truth.Truth.assertThat;
 
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import org.aion.crypto.ECKey;
+import org.aion.db.impl.SystemExitCodes;
+import org.aion.mcf.blockchain.Block;
 import org.aion.types.AionAddress;
-import org.aion.zero.impl.types.AionBlock;
+import org.aion.zero.impl.core.ImportResult;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 public class StandaloneBlockchainTest {
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Test
     public void testStandaloneBlockchainGenerateAccounts() {
@@ -24,5 +31,26 @@ public class StandaloneBlockchainTest {
             assertThat(bundle.bc.getRepository().getBalance(new AionAddress(k.getAddress())))
                     .isGreaterThan(BigInteger.ZERO);
         }
+    }
+
+    @Test
+    public void testBlockChainShutdownhook() {
+        exit.expectSystemExitWithStatus(SystemExitCodes.NORMAL);
+
+        StandaloneBlockchain.Builder builder = new StandaloneBlockchain.Builder();
+        StandaloneBlockchain.Bundle bundle =
+                builder.withValidatorConfiguration("simple").withDefaultAccounts().build();
+
+        StandaloneBlockchain sb = bundle.bc;
+
+        AionBlockchainImpl.shutdownHook = 2;
+        Block block1 = sb.createBlock(sb.genesis, Collections.EMPTY_LIST, false, TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
+        ImportResult result = sb.tryToConnect(block1);
+        assertThat(result.isBest()).isTrue();
+
+        Block block2 = sb.createBlock(block1, Collections.EMPTY_LIST, false, TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
+
+        // expect the system will be shutdown when import the block2
+        sb.tryToConnect(new BlockWrapper(block2));
     }
 }
