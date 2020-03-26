7314e524b203ff05c4f9ab3e24b9cd62af0ab73f
AionJayT <jay.tseng@aion.network>
2020-03-26 16:26:00 -0400

    Refactor synchrozied in the TrieImpl and Cache

--- modAionImpl/src/org/aion/zero/impl/trie/Cache.java
+++ modAionImpl/src/org/aion/zero/impl/trie/Cache.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 import org.aion.db.impl.ByteArrayKeyValueStore;
 import org.aion.log.AionLoggerFactory;
 import org.aion.log.LogEnum;
@@ -29,14 +30,21 @@ public class Cache {
     private Set<ByteArrayWrapper> removedNodes = new HashSet<>();
     private boolean isDirty;
 
+    private ReentrantLock lock = new ReentrantLock();
+
     public Cache(ByteArrayKeyValueStore dataSource) {
         this.dataSource = dataSource;
     }
 
-    public synchronized void markRemoved(byte[] key) {
-        ByteArrayWrapper keyW = ByteArrayWrapper.wrap(key);
-        removedNodes.add(keyW);
-        nodes.remove(keyW);
+    public void markRemoved(byte[] key) {
+        lock.lock();
+        try {
+            ByteArrayWrapper keyW = ByteArrayWrapper.wrap(key);
+            removedNodes.add(keyW);
+            nodes.remove(keyW);
+        } finally {
+            lock.unlock();
+        }
     }
 
     /**
@@ -47,125 +55,178 @@ public class Cache {
      * @param value the Node which could be a pair-, multi-item Node or single Value
      * @return keccak hash of RLP encoded node if length &gt; 32 otherwise return node itself
      */
-    synchronized void put(ByteArrayWrapper key, Value value) {
-        this.nodes.put(key, new Node(value, true));
-        this.removedNodes.remove(key);
-        this.isDirty = true;
+    void put(ByteArrayWrapper key, Value value) {
+        lock.lock();
+        try {
+            this.nodes.put(key, new Node(value, true));
+            this.removedNodes.remove(key);
+            this.isDirty = true;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized Value get(byte[] key) {
-
-        ByteArrayWrapper wrappedKey = wrap(key);
-        Node node = nodes.get(wrappedKey);
-        if (node != null) {
-            // cachehits++;
-            return node.getValue();
-        }
-        if (this.dataSource != null) {
-            Optional<byte[]> data = this.dataSource.get(key);
-            if (data.isPresent()) {
-                // dbhits++;
-                Value val = fromRlpEncoded(data.get());
-                nodes.put(wrappedKey, new Node(val, false));
-                return val;
+    public Value get(byte[] key) {
+        lock.lock();
+        try {
+            ByteArrayWrapper wrappedKey = wrap(key);
+            Node node = nodes.get(wrappedKey);
+            if (node != null) {
+                // cachehits++;
+                return node.getValue();
             }
-        }
+            if (this.dataSource != null) {
+                Optional<byte[]> data = this.dataSource.get(key);
+                if (data.isPresent()) {
+                    // dbhits++;
+                    Value val = fromRlpEncoded(data.get());
+                    nodes.put(wrappedKey, new Node(val, false));
+                    return val;
+                }
+            }
 
-        return null;
+            return null;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void commit(boolean flushCache) {
-        // Don't try to commit if it isn't dirty
-        if ((dataSource == null) || !this.isDirty) {
-            // clear cache when flush requested
+    public void delete(byte[] key) {
+        lock.lock();
+        try {
+            ByteArrayWrapper wrappedKey = wrap(key);
+            this.nodes.remove(wrappedKey);
+
+            if (dataSource != null) {
+                this.dataSource.delete(key);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void commit(boolean flushCache) {
+        lock.lock();
+        try {
+            // Don't try to commit if it isn't dirty
+            if ((dataSource == null) || !this.isDirty) {
+                // clear cache when flush requested
+                if (flushCache) {
+                    this.nodes.clear();
+                }
+                return;
+            }
+
+            // long start = System.nanoTime();
+            // int batchMemorySize = 0;
+            Map<byte[], byte[]> batch = new HashMap<>();
+            List<byte[]> deleteBatch = new ArrayList<>();
+            for (ByteArrayWrapper nodeKey : this.nodes.keySet()) {
+                Node node = this.nodes.get(nodeKey);
+
+                if (node == null || node.isDirty()) {
+                    byte[] value;
+                    if (node != null) {
+                        node.setDirty(false);
+                        value = node.getValue().encode();
+                    } else {
+                        value = null;
+                    }
+
+                    byte[] key = nodeKey.toBytes();
+
+                    batch.put(key, value);
+                    // batchMemorySize += length(key, value);
+                }
+            }
+            for (ByteArrayWrapper removedNode : removedNodes) {
+                deleteBatch.add(removedNode.toBytes());
+            }
+
+            this.dataSource.putBatch(batch);
+            this.dataSource.deleteBatch(deleteBatch);
+            this.isDirty = false;
             if (flushCache) {
                 this.nodes.clear();
             }
-            return;
+            this.removedNodes.clear();
+        } finally {
+            lock.unlock();
         }
-
-        // long start = System.nanoTime();
-        // int batchMemorySize = 0;
-        Map<byte[], byte[]> batch = new HashMap<>();
-        List<byte[]> deleteBatch = new ArrayList<>();
-        for (ByteArrayWrapper nodeKey : this.nodes.keySet()) {
-            Node node = this.nodes.get(nodeKey);
-
-            if (node == null || node.isDirty()) {
-                byte[] value;
-                if (node != null) {
-                    node.setDirty(false);
-                    value = node.getValue().encode();
-                } else {
-                    value = null;
-                }
-
-                byte[] key = nodeKey.toBytes();
-
-                batch.put(key, value);
-                // batchMemorySize += length(key, value);
-            }
-        }
-        for (ByteArrayWrapper removedNode : removedNodes) {
-            deleteBatch.add(removedNode.toBytes());
-        }
-
-        this.dataSource.putBatch(batch);
-        this.dataSource.deleteBatch(deleteBatch);
-        this.isDirty = false;
-        if (flushCache) {
-            this.nodes.clear();
-        }
-        this.removedNodes.clear();
     }
 
-    public synchronized boolean isDirty() {
-        return isDirty;
+    public boolean isDirty() {
+        lock.lock();
+        try {
+            return isDirty;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized Map<ByteArrayWrapper, Node> getNodes() {
-        return nodes;
+    public Map<ByteArrayWrapper, Node> getNodes() {
+        lock.lock();
+        try {
+            return nodes;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized ByteArrayKeyValueStore getDb() {
-        return dataSource;
+    public ByteArrayKeyValueStore getDb() {
+        lock.lock();
+        try {
+            return dataSource;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @SuppressWarnings("OptionalGetWithoutIsPresent")
-    public synchronized void setDB(ByteArrayKeyValueStore kvds) {
-        if (this.dataSource == kvds) {
-            return;
-        }
+    public void setDB(ByteArrayKeyValueStore kvds) {
+        lock.lock();
+        try {
+            if (this.dataSource == kvds) {
+                return;
+            }
 
-        Map<byte[], byte[]> rows = new HashMap<>();
-        if (this.dataSource == null) {
-            for (ByteArrayWrapper key : nodes.keySet()) {
-                Node node = nodes.get(key);
-                if (node == null) {
-                    rows.put(key.toBytes(), null);
-                } else if (!node.isDirty()) {
-                    rows.put(key.toBytes(), node.getValue().encode());
+            Map<byte[], byte[]> rows = new HashMap<>();
+            if (this.dataSource == null) {
+                for (ByteArrayWrapper key : nodes.keySet()) {
+                    Node node = nodes.get(key);
+                    if (node == null) {
+                        rows.put(key.toBytes(), null);
+                    } else if (!node.isDirty()) {
+                        rows.put(key.toBytes(), node.getValue().encode());
+                    }
+                }
+            } else {
+                Iterator<byte[]> iterator = dataSource.keys();
+                while (iterator.hasNext()) {
+                    byte[] key = iterator.next();
+                    rows.put(key, this.dataSource.get(key).get());
+                }
+
+                try {
+                    this.dataSource.close();
+                } catch (Exception e) {
+                    LOG.error("Unable to close data source.", e);
                 }
             }
-        } else {
-            Iterator<byte[]> iterator = dataSource.keys();
-            while (iterator.hasNext()) {
-                byte[] key = iterator.next();
-                rows.put(key, this.dataSource.get(key).get());
-            }
 
-            try {
-                this.dataSource.close();
-            } catch (Exception e) {
-                LOG.error("Unable to close data source.", e);
-            }
+            kvds.putBatch(rows);
+            this.dataSource = kvds;
+        } finally {
+            lock.unlock();
         }
-
-        kvds.putBatch(rows);
-        this.dataSource = kvds;
     }
 
     public int getSize() {
-        return nodes.size();
+        lock.lock();
+        try {
+            return nodes.size();
+        } finally {
+            lock.unlock();
+        }
     }
 }
